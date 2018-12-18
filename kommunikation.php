30634209574b7edd418eb83d10051e10ec9607fe
Soren Jessen <soer64b9@student.eadania.dk>
2018-12-18 09:09:59 +0100

    Update kommunikation.php

---
+++ kommunikation.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html> 
+<html lang="da">
+    <head> 
+        
+        <title> Kommunikation</title>
+        <?php include "overordnet.inc" ?>    
+        <link rel="stylesheet" href="kommunikation.css">
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+        
+        <div id="pmodel"> <!--Produktionsmodeller box start-->
+            <div id="pmtekst">
+                <h1> Kommunikation<br>og Produktions-<br>modeller</h1>
+                <p>Paradigmer, Laswell, AIDA osv. </p>
+            </div>
+                <div id="pmbillede">    
+                    <img id="pmbillede" class="allebilleder" src="Billeder/pmbillede.jpg"> 
+                </div>
+        </div> <!--Produktionsmodeller box slut-->
+            
+        <div id="pteknik"> <!--Præsentationsteknik box start-->
+            <div id="ptekniktekst">
+                <h1> Præsentations-<br>teknik </h1>
+                <p> Elevatortale, Dispositio, Taler, Kropssprog osv.</p>
+            </div>
+                <div id="ptbillede">    
+                    <img id="ptbillede" class="allebilleder" src="Billeder/ptbillede.jpg"> 
+                </div>
+        </div> <!--Præsentationsteknik box slut-->    
+        
+        <div id="tproduktion"> <!--tekstproduktion box start-->
+            <div id="tproduktiontekst">
+                <h2> Tekstproduktion</h2>
+                <p> Find din indre "Jørgen Forfatter" frem.</p>
+            </div>
+                <div id="tproduktionbillede">
+                    <img id="tproduktionbillede" class="allebilleder" src="Billeder/tproduktionbillede.jpg">
+                </div>
+        </div> <!--Tekstproduktion box slut-->
+    
+        <div id="bruger"> <!--Brugertest box start-->
+            <div id="brugertekst">
+                <h2> Brugertest</h2>
+                <p> Videnskabsteori, Kortsortering, gangstertest osv.</p>
+                
+            </div>
+                <div id="brugerbillede">
+                    <img id="brugerbillede" class="allebilleder" src="Billeder/brugerbillede.jpg">
+                </div>
+        </div>  <!--Brugertest box slut-->
+        
+        <div id="bmc"> <!--BMC box start-->
+            <div id="bmctekst">
+                <h2> Business Model Canvas </h2>
+                <p> Forretnings-<br>forståelse, strategisk analyse osv.</p>
+                
+            </div>
+                <div id="bmcbillede">
+                    <img id="bmcbillede" class="allebilleder" src="Billeder/bmcbillede.jpg">
+                </div>    
+        </div> <!--BMC box slut-->
+    
+            
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
