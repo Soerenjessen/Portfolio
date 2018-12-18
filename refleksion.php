b6a93cf3c941012c2bce16c76ad6db2b8f8531a0
Soren Jessen <soer64b9@student.eadania.dk>
2018-12-18 09:10:08 +0100

    Update refleksion.php

---
+++ refleksion.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html> 
+<html lang="da">
+    <head> 
+        
+        <title> Refleksion</title>
+        <?php include "overordnet.inc" ?>    
+        <link rel="stylesheet" href="#"> <!--unikt Stylesheet mangler-->
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+        
+      <h1> Refleksion</h1>
+            
+        <p> Mere tekst på vej</p>    
+            
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
