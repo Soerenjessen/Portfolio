f99217f2c9d9cdda374fb180d288b51e1ac619c8
Soren Jessen <soer64b9@student.eadania.dk>
2018-12-18 09:10:04 +0100

    Update online.php

---
+++ online.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html> 
+<html lang="da">
+    <head> 
+        
+        <title> Online Markedsføring</title>
+        <?php include "overordnet.inc" ?>    
+        <link rel="stylesheet" href="#"> <!--unikt Stylesheet mangler-->
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+        
+      <h1> Online Markedsføring</h1>
+            
+        <p> Mere tekst på vej</p>    
+            
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
