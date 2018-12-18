190c79d5f42b9d56a3d0bee3ca7b37806726874f
Soren Jessen <soer64b9@student.eadania.dk>
2018-12-18 09:09:40 +0100

    Update design.php

---
+++ design.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html> 
+<html lang="da">
+    <head> 
+        
+        <title> Design</title>
+        <?php include "overordnet.inc" ?>    
+        <link rel="stylesheet" href="CSS/design.css">
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+        
+        <div id="illustrator"> <!--Illustrator box start-->
+            <div id="itekst">
+                <h1> Illustrator</h1>
+                <p> Vectorgrafik, Artboards, logoer, Figurer, Layer Masks osv.</p>
+            </div>
+                <div id="ibillede">    
+                    <img id="ibillede" class="allebilleder" src="Billeder/ibillede.jpg"> 
+                </div>
+        </div> <!--Illustrator box slut-->
+            
+        <div id="ps"> <!--Photoshop box start-->
+            <div id="pstekst">
+                <h1> Photoshop </h1> 
+                <p> Billedebehandling, Manipulation, Pixels, Artboards, Layer osv.</p>
+            </div>
+                <div id="psbillede">    
+                    <img id="psbillede" class="allebilleder" src="Billeder/psbillede.jpg"> 
+                </div>
+        </div> <!--Photoshop box slut-->    
+        
+        <div id="fotografering"> <!--Fotografering box start-->
+            <div id="fototekst">
+                <h2> Fotografering</h2>
+                <p> Lukketid, ISO, Blænde, lukkevide, Lys osv.</p>
+            </div>
+                <div id="fotobillede">
+                    <img id="fotobillede" class="allebilleder" src="Billeder/fotobillede.jpg">
+                </div>
+        </div> <!--Fotografering box slut-->
+    
+        <div id="gestalt"> <!--Gestaltlove box start-->
+            <div id="gestalttekst">
+                <h2> Gestaltlove</h2>
+                <p> Proximity, Continuity, Simalarity, Perception osv.</p>
+                
+            </div>
+                <div id="gestaltbillede">
+                    <img id="gestaltbillede" class="allebilleder" src="Billeder/gestaltbillede.jpg">
+                </div>
+        </div>  <!--Gestaltlove box slut-->
+        
+        <div id="farve"> <!--Farve og typografi box start-->
+            <div id="farvetekst">
+                <h2> Farve og Typografi </h2>
+                <p> Farvehjul, Primær Farver, RGB, CMYK, Fonts, Weights, </p>
+                
+            </div>
+                <div id="farvebillede">
+                    <img id="farvebillede" class="allebilleder" src="Billeder/farvebillede.jpg">
+                </div>    
+        </div> <!--Farve og Typografi box slut-->
+    
+            
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
