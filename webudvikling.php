915ef5e3ffc02d8943ead8f176b752cf8b480025
Soren Jessen <soer64b9@student.eadania.dk>
2018-12-18 09:10:15 +0100

    Update webudvikling.php

---
+++ webudvikling.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html> <!--hallo-->
+<html lang="da">
+    <head> 
+        
+        <title> Webudvikling</title>
+        <?php include "overordnet.inc" ?>    
+        <link rel="stylesheet" href="CSS/webudvikling.css">
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+        
+        <a href="tml.php"><div id="tml"> <!--HTML box start-->
+            <div id="tmltekst">
+                <h1> HTML</h1>
+                <p> Semantiske tags, Struktur, Elementer, Kodeskik, Editor osv</p>
+            </div>
+                <div id="tmlbillede">    
+                    <img id="tmlbillede" class="allebilleder" src="Billeder/htmlbillede.jpg"> 
+                </div>
+        </div></a> <!--HTML box slut-->
+            
+        <div id="historie"> <!--internettets historie box start-->
+            <div id="historietekst">
+                <h1> Internettets Historie</h1>
+                <p> Historien bag World Wide Web og dens pionerer </p>
+            </div>
+                <div id="historiebillede">    
+                    <img id="historiebillede" class="allebilleder" src="Billeder/historiebillede.jpg"> 
+                </div>
+        </div> <!--Internettets historie box slut-->    
+        
+        <div id="webserver"> <!--webserver box start-->
+            <div id="servertekst">
+                <h2> Webserver</h2>
+                <p> Her kan du finde information om Server, Klienter, FTP Server osv </p>
+            </div>
+                <div id="serverbillede">
+                    <img id="serverbillede" class="allebilleder" src="Billeder/serverbillede.jpg">
+                </div>
+        </div> <!--webserver box slut-->
+    
+        <div id="css"> <!--CSS box start-->
+            <div id="csstekst">
+                <h2> CSS</h2>
+                <p> Kaskaderegler, Styling, Layout, Class, Id, Selectors osv</p>
+                
+            </div>
+                <div id="cssbillede">
+                    <img id="cssbillede" class="allebilleder" src="Billeder/cssbillede.jpg">
+                </div>
+        </div>  <!--CSS box slut-->
+        
+        <div id="planning"> <!--Planlægning box start-->
+            <div id="plantekst">
+                <h2> Planlægning og Overordnet Design</h2>
+                <p> Det Bette Ismejeri</p>
+                
+            </div>
+                <div id="planbillede">
+                    <img id="planbillede" class="allebilleder" src="Billeder/planbillede.jpg">
+                </div>    
+        </div> <!--Planlægning box slut-->
+    
+        <div id="workshop"> <!--Workshop box start-->
+            <div id="worktekst">   
+                <h2> HTML og CSS Workshop</h2>
+                <p> HTML Tabeller og Styling af dem</p>
+                
+            </div>
+                <div id="workbillede">
+                    <img id="workbillede" class="allebilleder" src="Billeder/workbillede.jpg">
+                </div>    
+        </div> <!--Workshop box slut-->
+        
+        <div id="grids"> <!--CSS Grid box start-->
+            <div id="gridtekst">
+                <h2> CSS Grid</h2>
+                <p> Responsive Webdesign, Grid Container, Mobile First</p>
+            
+            </div>
+                <div id="gridbillede">
+                    <img id="gridbillede" class="allebilleder" src="Billeder/gridbillede.jpg">
+                </div>    
+        </div> <!--CSS Grid box slut-->
+            
+        <div id="version"> <!--Versionsstyring box start-->
+            <div id="versiontekst">
+                <h2> Versionsstyring</h2>
+                <p> Github, Protokoller, projektmapper, Commits osv</p>
+            
+            </div>
+                <div id="versionbillede">
+                    <img id="versionbillede" class="allebilleder" src="Billeder/versionbillede.jpg">
+                </div>    
+        </div> <!--Versionsstyring box slut-->    
+            
+        <div id="ia"> <!--Informationsarkitektur box start-->
+            <div id="iatekst">
+                <h2> Informations-<br>arkitektur</h2>
+                <p> Organisering og Struktur af information</p>
+            
+            </div>
+                <div id="iabillede">
+                    <img id="iabillede" class="allebilleder" src="Billeder/iabillede.jpg">
+                </div>    
+        </div> <!--Informationsarkitektur box slut-->    
+            
+        <div id="onepage"> <!--One page box start-->
+            <div id="onetekst">
+                <h2> One Page</h2>
+                <p> Biotex Bent og One Page Projektet</p>
+            
+            </div>
+                <div id="onebillede">
+                    <img id="onebillede" class="allebilleder" src="Billeder/onebillede.jpg">
+                </div>    
+        </div> <!--One page box slut-->
+            
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
