130a0b5734ebe554691ab38ab33dfd44fdd2f6eb
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:07:54 +0100

    Create illustrator.php

---
+++ illustrator.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head> 
+        
+        <title>Illustrator</title>
+        <?php include "overordnet.inc" ?>
+        <link rel="stylesheet" href="CSS/illustrator.css">
+
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+
+        <div id="illustrator"> <!--Illustrator start-->
+            <h1>Illustrator</h1> 
+            
+            <p class="tekst"> Illustrator er et vectorbaseret tegneprogram, som vi har brugt til at tegne logoer og figurer. Det smarte ved at arbejde med vectorgrafik er, at man kan skalere sit design uendeligt uden at kvaliteten går tabt. Det kan være en fordel, hvis man tegner et logo, som skal bruges på alt lige fra t-shirts til en stor ende gavl.</p> 
+            
+            <p class="tekst"> I Illustrator arbejder man i lag og artboards for at kunne arbejde non-destruktivt. </p> 
+            
+            <p class="tekst"> Shape tool bruges til at skabe en figur. </p> 
+            
+            <p class="tekst"> Custom shape tool er et værktøj, som bruges til at samle flere shapes til en samlet figur.</p> 
+            
+        
+        <div id="flag1container">
+            
+                <img id="flag1" class="allebilleder" src="Billeder/flag1.jpg">
+                
+                <blockquote>Det første flag vi designede i illustrator ↑</blockquote>            
+        </div>     
+                
+              
+        </div>  <!--Illustrator slut-->
+            
+        <div id="logo"> <!--logo start-->
+            <h2>Multimediedesigner Logo i Illustrator</h2> 
+            
+                <p class="tekst"> Til at skabe dette logo har vi anvendt bl.a. Elipse-tool til at gøre cirklen rund, og guidelines til at placere elementer de rigtige steder i artboardet. Derudover har jeg bygget den i forskellige layers, dvs non-destruktivt. </p>
+            
+            <div id="logocontainer">
+            
+                <img id="forstelogo" class="allebilleder" src="Billeder/forstelogo.jpg">
+                
+                <blockquote>Det første logo ↑</blockquote>            
+            </div> 
+            
+        </div>  <!--logo slut-->       
+        
+            
+        <div id="logo2"> <!--logo2 start-->
+            <h2>Captain America Logo</h2> 
+            
+                <p class="tekst"> Den første dag vi arbejdede med illustrator skulle vi tegne dette logo. Opgaven gik ud på at anvende bl.a. ellipse-tool</p>
+            
+                <p class="tekst"> Derudover er logoet bygget op i forskellige layers.</p>        
+            
+            <div id="kaptajncontainer">
+            
+                <img id="kaptajn" class="allebilleder" src="Billeder/kaptajn.jpg">
+                
+                <blockquote>logo tegnet med ellipse-tool ↑</blockquote>            
+            </div> 
+            
+        </div>  <!--logo2 slut-->        
+    
+            
+            <div id="felt19"> &nbsp;</div> <!--holder footer nede på siden-->     
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
