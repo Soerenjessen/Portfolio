de4063f65387d23781039cdce422bfaef9f6e0a0
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:07:59 +0100

    Create kilder.php

---
+++ kilder.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html> <!--hallo-->
+<html lang="da">
+    <head> 
+        
+        <title> Kilder</title>
+        <?php include "overordnet.inc" ?>    
+        <link rel="stylesheet" href="CSS/webudvikling.css">
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+        
+        <div id="kilder"> <!--Kilder start-->
+                <h1> Kilder</h1>
+                <p> Kildeliste og bibliografi</p>
+        </div> <!--Kilder slut-->
+            
+            <div id="felt18"> &nbsp;</div> <!--holder footer nede på siden-->     
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
