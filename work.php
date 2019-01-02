187ffcc9c7bcb8fdbc66f482547412f01abc86bf
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:09:01 +0100

    Create work.php

---
+++ work.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head> 
+        
+        <title> HTML og CSS Workshop</title>
+        <?php include "overordnet.inc" ?>
+        <link rel="stylesheet" href="CSS/work.css">
+
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+
+        <div id="indledning"> <!--Indledning start-->
+            <h1> HTML og CSS Workshop</h1> 
+            
+            <p class="tekst"> I forbindelse med HTML og CSS Workshop skulle jeg arbejde med HTML-tabeller, og hvordan vi kan style dem i CSS. På det tidspunkt jeg var blevet stillet opgaven, havde jeg ingen kendsskab til CSS grid, som f.eks. denne her hjemmeside er bygget med. I dag vil jeg foretrække at bruge CSS-grids i stedet for HTML-tabeller. </p> 
+            
+            <p class="tekst"> Jeg har valgt at tage udgangspunkt i rapporten jeg skrev i forbindelse med projektet. Du kan læse hele rapporten ved at klikke på billedet længere nede.</p>
+
+        </div>  <!--Indledning slut-->  
+            
+        <div id="billedetabelcontainer"> <!--billedelink start-->
+            
+            <a href="HTML%20Tables-kopi.pdf"><img id="tabelbillede" class="allebilleder" src="Billeder/tabelbillede.jpg" alt="HTML Tabeller og styling"></a>
+            
+            <blockquote> Læs hele rapporten her - Klik på billedet ↑ </blockquote> 
+                        
+        </div> <!--billedelink slut-->   
+            
+            
+        <div id="refleks"> <!--Refleksion start-->
+            <h2>Læring og Refleksion </h2> 
+            
+            <p class="tekst"> Dette var det første større selvstændige projekt, som jeg skulle arbejde med. Jeg synes, at jeg fik rigtigt meget ud af processen, da jeg var tvunget til at skulle sætte mig ind i tingene. Jeg brugte meget tid på W3 School, Lynda, Youtube osv. </p>
+            
+            <p class="tekst">Til sidst skulle jeg aflevere et samlet dokument over mine notater og eksempler. Dette skulle bruges til et samlet PDF-opslagsværk, som vi skulle kunne bruge på holdet.  
+                
+            <p class="tekst"> Afslutningsvis skulle jeg forberede en fremlæggelse for klassen, og fortælle om hvad jeg havde lært og HTML-tabeller / Styling af dem. </p>
+
+        </div>  <!--Refleksion slut-->  
+            
+            
+            <div id="felt7"> &nbsp;</div> <!--holder footer nede på siden-->
+        
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
