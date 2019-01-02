060ab4cf0113c28323e7a0c340402723e5601c77
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:07:34 +0100

    Create gestaltlove.php

---
+++ gestaltlove.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head> 
+        
+        <title>Gestaltlove</title>
+        <?php include "overordnet.inc" ?>
+        <link rel="stylesheet" href="CSS/gestaltlove.css">
+
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+
+            
+        <div id="gestaltlov"> <!--gestaltlov start-->
+            <h1>Gestaltlove</h1> 
+            
+            <p class="tekst"> Gestaltlovene er et designværktøj, som vi bruger til at forstå hvordan den menneskelige hjerne ser sammenhænge i omverdenen, og hvordan vi sanser og føler.</p> 
+            
+            <p class="tekst"> Gestaltlovene handler om perception og vores opfattelse af det vi sanser. Perception er påvirket af gestaltlovene samt vores baggrund og forventninger. </p> 
+            
+            <p class="tekst"> Gestaltlovene handler om at se helheder, og samtidig skabe overblik for brugerne. Her kan man anvende de forskellige gestaltlove til at organisere indhold og informationer, så brugeren lettere kan overskue indholdet.</p> 
+              
+            <p class="tekst"> Med Gestaltlovene arbejder man på at få funktionalitet og design til at gå op i en højere enhed.</p> 
+            
+            
+         <div id="gestalt1container">
+            
+                <img id="gestalt1" class="allebilleder" src="Billeder/gestalt1.jpg">
+                
+                <blockquote>Eksempel på perception ↑</blockquote>            
+        </div>     
+                
+        </div>  <!--gestaltlov slut-->
+        
+            
+        <div id="perception"> <!--perception start-->
+            <h2> Perception </h2> 
+            
+            <p class="tekst">Perception handler om opfattelse af noget via sanserne. Det vores opfattelse af det vi sanser. Perception er påvirket af gestaltlovene samt vores baggrund og forventninger.  </p>
+            
+            <p class="tekst"> Objektperception er vores opfattelse af ydre objekter.</p>
+            
+            <p class="tekst"> Eksempel på perception. I starten ser vi ikke hunden, men blot sorte former på en hvid baggrund, men vi danner hurtigt et samlet billede af en hund der nysgerrigt snuser til jorden.</p>
+            
+        
+        <div id="dalmatinercontainer">
+            
+                <img id="dalmatiner" class="allebilleder" src="Billeder/dalmatiner.jpg">
+                
+                <blockquote>Eksempel på perception ↑</blockquote>            
+        </div>     
+            
+        </div>  <!--perception slut-->
+            
+            
+        <div id="proximity"> <!--Proximity start-->
+            <h2> Proximity (Nærhed) </h2> 
+            
+            <p class="tekst"> Objekter som er tætte, opfattes (perception) som værende relaterede, i højere grad end objekter som er placeret langt fra hinanden.</p>
+            
+            <p class="tekst">Nærhed trumfer lighed i blandet farver og kontraster. </p>
+            
+            <p class="tekst">Det er et af de første principper som rammer vores opfattelsesevne.(Bogner, 2018, S.10 ,04_gestaltlovene.pdf) </p>
+            
+            <p class="tekst">Bemærk: Man arbejder i hovedreglen med mere end en gestaltlov. Man bruger ofte flere gestaltlove sammen.</p>
+            
+            
+        <div id="proximity1container">
+            
+                <img id="proximity1" class="allebilleder" src="Billeder/proximity1.jpg">
+                
+                <blockquote>Eksempel på proximity ↑</blockquote>            
+        </div>  
+            
+        </div>  <!--Proximity slut-->    
+        
+            
+        <div id="continuity"> <!--Continuity  start-->
+            <h2> Continuity (Kontinuitet)</h2> 
+            
+            <p class="tekst"> Objekter arrangeret på linie eller kurver, opfattes som relaterede i højere grad end hvis de ikke er.</p>
+            
+            <p class="tekst"> Brødkrumme, paragraffer, grafer (både vertikale og horisontale) kommunikerer relation. (Bogner, 2018, S.10 ,04_gestaltlovene.pdf)</p>
+            
+        <div id="continuitycontainer">
+            
+                <img id="kontinuitet" class="allebilleder" src="Billeder/kontinuitet.jpg">
+                
+                <blockquote>Reglen om Kontinuitet ↑</blockquote>            
+        </div>     
+            
+        </div>  <!--Continuity  slut-->    
+        
+            
+        <div id="simalarity"> <!--Simalarity  start-->
+            <h2> Simalarity (Lighed) </h2> 
+            
+            <p class="tekst"> Visuelle elementer som er ens i form, størrelse, farve og retning opfattes som del af en gruppe. 
+            Farver er den stærkeste måde at skabe lighed på. 
+            Det kan benyttes til at skabe relationer i navigation, links osv. for at understøtte hierarkiet i sitet. 
+            Gennemført styling og content er vigtigt og antyder struktur. (Bogner, 2018, S.10 ,04_gestaltlovene.pdf)</p>
+            
+            <p class="tekst"> Similarity er en lov, som er med til angive struktur.</p>
+            
+        <div id="lighedcontainer">
+            
+                <img id="lighed" class="allebilleder" src="Billeder/lighed.jpg">
+                
+                <blockquote>Reglen om lighed ↑</blockquote>            
+        </div>  
+                    
+            
+        </div>  <!--Simalarity  slut-->    
+            
+            
+            
+        <div id="simplicity"> <!--Simplicity  start-->
+            <h2> Simplicity </h2> 
+            
+            <p class="tekst"> Når man arbejder med simplicity handler det om at skabe nytte. Man skal fjerne alt, som kan distrahere brugeren, og ingen funktion har, men blot er med for at pynte. </p>
+            
+            <p class="tekst"> I forhold til en hjemmeside handler det om at designe siden, så det er nemt for brugerne at finde rundt på siden og finde det væsentlige. Siden skal altså være nem at bruge. </p>
+            
+            <p class="tekst"> Man kan opnå simplicity ved at arbejde med farver, skrifttyper og grafiske løsninger. </p>
+            
+            <p class="tekst"> -	Farver: Brug max. 5 forskellige farver. </p>
+            
+            <p class="tekst"> -	Skrifttyper: Brug højst 2 forskellige skrifttyper i max. 3 størrelser. (Sans Serif til overskrifter og serif til brødtekst). </p>
+            
+            <p class="tekst">  - Grafiske løsninger: Bør ikke bruges til at pynte med, men derimod til at gøre det nemmere for brugeren – Brug ikoner og symboler til at navigere i indholdet.</p>
+            
+            
+        <div id="enkelthedcontainer">
+            
+                <img id="enkelthed" class="allebilleder" src="Billeder/enkelthed.jpg">
+                
+                <blockquote>Reglen om simplicity ↑</blockquote>            
+        </div>  
+                    
+            
+        </div>  <!--Simplicity  slut-->        
+            
+            
+        <div id="whitespace"> <!--white space start-->
+            <h2> White Space </h2> 
+            
+            <p class="tekst"> White space er den luft som er imellem billeder, tekst, elementer osv. White space bruges som et grafisk trick til enten at knytte elementer sammen, eller adskille dem.</p>
+            
+            <p class="tekst"> Udover at bidrage til den overordnede æstetik, er white space også med til at øge læsbarheden af dine tekster på en side, ved at skabe overblik og forståelse. </p>
+            
+        <div id="ipadcontainer">
+            
+                <img id="ipad" class="allebilleder" src="Billeder/ipad.jpg">
+                
+                <blockquote>Apple er vildt dygtige til at bruge white space ↑</blockquote>            
+        </div>  
+                    
+        </div>  <!--white space slut-->   
+            
+            
+        <div id="rules"> <!--Rule of Thirds start-->
+            <h2>Rule of Thirds</h2> 
+            
+            <p class="tekst"> Rule of thirds er en teori, som man bruger til at komponere et billede. Det bruges til at skabe balance i et billede, og kan bruges som grafisk retningslinje, da det er med til at sætte fokus på nogle særlige punkter på et billede.</p>
+            
+            <p class="tekst"> Man opdeler et billede i 9 lige store felter. To streger trækkes vandret og lodret, og på den måde skaber man nogle skæringspunkter.</p>
+            
+        <div id="fyrcontainer">
+            
+                <img id="fyr" class="allebilleder" src="Billeder/fyr.jpg">
+                
+                <blockquote>Dette billede er inddelt efter Rule of Thirds ↑</blockquote>            
+        </div>  
+                    
+        </div>  <!--Rule of Thirds slut-->     
+            
+            
+         <div id="figur"> <!--Figure Ground start-->
+            <h2>Figure Ground</h2> 
+            
+            <p class="tekst"> Figure ground handler om at skelne imellem forgrunden og baggrunden på et billede.</p> 
+                
+            <p class="tekst">  Det kan være svært for det menneskelige øje at differentiere imellem baggrund og forgrund. </p> 
+                
+            <p class="tekst"> I sidste ende er det meget individuelt, hvordan man opfatter baggrund og forgrund.</p>
+             
+            <p class="tekst"> Nogle mennesker vil se to ansigter mod hinanden, og andre vil se en vase.</p>
+            
+        <div id="figurecontainer">
+            
+                <img id="figure" class="allebilleder" src="Billeder/figure.jpg">
+                
+                <blockquote>Eksempel med to ansigter og en vase ↑</blockquote>            
+        </div>  
+                    
+        </div>  <!--Figure Ground slut-->       
+            
+            
+          <div id="common"> <!--Common Fate start-->
+            <h2>Common Fate</h2> 
+            
+            <p class="tekst"> Common fate har fokus på elementer der enten bevæger sig i samme retning, eller har samme funktion. Det ligger lidt i navnet ”samme skæbne”.</p> 
+                
+            <p class="tekst"> Det er et mønster vi ofte ser i naturen blandt fugleflokke og fiskestimer, som også bevæger sig i samme retning.  </p> 
+                
+            <p class="tekst"> Vi opfatter derfor elementer som bevæger sig på samme måde, og i samme retning, som værende sammenhørende.</p>
+             
+
+        <div id="fiskestimecontainer">
+            
+                <img id="fiskestime" class="allebilleder" src="Billeder/fiskestime.jpg">
+                
+                <blockquote>Fiskestime som bevæger sig i samme retning ↑</blockquote>            
+        </div>  
+                    
+        </div>  <!--Common Fate slut-->       
+            
+            
+        <div id="gridz"> <!--Grids start-->
+            <h2>Grids</h2> 
+            
+            <p class="tekst"> Grids er et slags gitter som man bruger til at organisere sit indhold. Det minder meget om f.eks. css grids, som denne hjemmeside er opbygget med.</p> 
+                
+            <p class="tekst">Grids er med til at skabe struktur og en ensartethed. Det bliver i høj grad brugt i aviser, magasiner osv. Man bemærker kun grids, når det ikke er der. </p> 
+
+        <div id="gittercontainer">
+            
+                <img id="gitter" class="allebilleder" src="Billeder/gitter.jpg">
+                
+                <blockquote>Eksempel på grids i The Atlantic ↑</blockquote>            
+        </div>  
+                    
+        </div>  <!--Grids slut-->      
+            
+            
+            
+        <div id="golden"> <!--Golden start-->
+            <h2>Golden Section</h2> 
+            
+            <p class="tekst"> Den gyldne spiral et gammelt geometrisk begreb, som også kan findes i naturen.</p> 
+                
+            <p class="tekst">Den udspringer fra Fibonaccis talrække.</p> 
+
+        <div id="spiralcontainer">
+            
+                <img id="spiral" class="allebilleder" src="Billeder/spiral.jpg">
+                
+                <blockquote>Sneglehus med Den gyldne spiral ↑</blockquote>            
+        </div>  
+                    
+        </div>  <!--Grids slut-->      
+            
+            
+            
+        <div id="closure"> <!--Closure start-->
+            <h2>Closure</h2> 
+            
+            <p class="tekst"> Reglen om lukkethed handler om menneskers evne til at afslutte mønstre og figurer, som ikke er helt afsluttet eller hvor noget information mangler.</p> 
+                
+            <p class="tekst">Eksempel på dette er billedet nedenunder. Enten ser vi en hvid trekant oven på tre cirkler, ellers ser vi tre åbne trekanter</p> 
+
+        <div id="closecontainer">
+            
+                <img id="close" class="allebilleder" src="Billeder/close.jpg">
+                
+                <blockquote>Reglen om lukkethed ↑</blockquote>            
+        </div>  
+                     
+        </div>  <!--Closure slut-->        
+            
+                
+        <div id="symmetry"> <!--Symmetry start-->
+            <h2>Symmetry</h2> 
+            
+            <p class="tekst"> Når vi ser symmetriske figurer, antager vi ofte, at der er en form for sammenhæng imellem dem. I alt symmetri har man en symmetriakse, som fungerer ligesom et spejl. </p> 
+                
+            <p class="tekst">Vi kan spejle figurer horisontalt, vertikalt og diagonalt.</p> 
+               
+            <p class="tekst">Den menneskelige hjerne forbinder symmetriske mønstre og figurer med ro, struktur og stabilitet, hvorimod asymmetriske figurer givet et mere negativt indtryk såsom ”ude af balance”, ”noget er galt”, ”noget mangler”.</p>    
+
+        <div id="symmetriccontainer">
+            
+                <img id="symmetric" class="allebilleder" src="Billeder/symmetric.jpg">
+                
+                <blockquote>Reglen om symmetri ↑</blockquote>            
+        </div>  
+                        
+        </div>  <!--Symmetry slut-->        
+                
+            
+            <div id="felt22"> &nbsp;</div> <!--holder footer nede på siden-->     
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
