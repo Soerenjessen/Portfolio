cefdd102cf182e0eeff3edff3227261058e5624e
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:08:23 +0100

    Create plan.php

---
+++ plan.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head> 
+        
+        <title> Planlægning og Overordnet Design</title>
+        <?php include "overordnet.inc" ?>
+        <link rel="stylesheet" href="CSS/plan.css">
+
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+
+        <div id="planl"> <!--Planlægning og Overordnet design start-->
+            <h1> Planlægning og Overordnet Design</h1> 
+            
+            <p class="tekst"> I planlægning og overordnet design har vi arbejdet med, hvordan man kan bruge forskellige teknikker og processor til at opnå et bedre resultat i forhold til et webudviklingsprojekt. </p> 
+            
+            <p class="tekst"> I den forbindelse holdte vi et fiktivt kundemøde med Niels, som i dagens anledning var blevet ejer af ismejeriet, Det Bette Ismejeri. Her skulle vi aktivt bruge de metoder og værktøjer, som vi havde læst om til den dag.</p> 
+                
+            <p class="tekst">Vi brugte bl.a. Goals and Objectives diskussion til at skabe et overblik over, hvilke tanker og ideer, Niels havde gjort sig omkring hjemmesiden til Det Bette Ismejeri. Vi spurgte ind til, hvad målet var med hjemmesiden, og hvilket formål den skulle tjene. Derudover bad vi ham om at fortælle omkring sig selv, og Det Bette Ismejeri, for at blive klogere på hele projektet. Grundlæggende set fandt vi ud af, at han gerne ville have en hjemmeside, da han ikke havde nogen i forvejen. Tilmed gav han også udtryk for, at han ikke var vild med logoet</p>  
+            
+            <p class="tekst"> Det var vigtigt for os at få stillet de rigtige spørgsmål til Mejeristen Niels, da han havde utrolig svært at beskrive, hvor han gerne ville hen med sin hjemmeside. Efter denne samtale, hvor vi løbende havde skrevet notater, var vi blevet en del klogere på, hvad der var vigtigt for ham.</p> 
+                    
+         </div>  <!--Planlægning og Overordnet design slut--> 
+        
+            
+        <div id="brainstorm"> <!--Brainstorm start-->
+            <h1> Brainstorm </h1> 
+            
+            <p class="tekst"> Ud fra den samtale vi havde med Niels, gik vi i gang med en brainstormproces, hvor vi begyndte at arbejde divergent med ideer. Her handlede det om at komme op med alle de ideer vi havde, der skulle også være plads til de vilde ideer</p>
+            
+            <p class="tekst"> Herefter begyndte vi arbejde mere konvergent med vores ideer, altså sortere de ideer fra, som ikke var gode nok. </p>
+            
+        <div id="isbraincontainer">
+            
+            <img id="isbrain" class="allebilleder" src="Billeder/isbrain.jpg">
+            
+            <blockquote> Brainstorm til Det Bette Ismejeri</blockquote>
+            
+        </div>    
+            
+            
+        </div>  <!--Brainstorm slut-->
+            
+        <div id="user"> <!--User Stories start-->
+            <h1> User Stories </h1> 
+            
+            <p class="tekst"> Efter vi havde sorteret i vores ideer fra brainstormprocessen, begyndte vi at udarbejde User Stories. Fordelen ved User Stories er, at det giver et fælles sprog imellem alle involverede parter. Det eksempel vi brugte var (Som en bruger, skal/kan/vil/må, jeg ”handling” så jeg ”formål”). </p>   
+            
+        <div id="planusercontainer">
+            
+            <img id="planuser" class="allebilleder" src="Billeder/planuser.jpg">
+            
+            <blockquote> Eksempel: User Story fra One Page-projektet</blockquote>
+            
+        </div> 
+            
+            
+        </div>  <!--User stories Tags slut-->
+            
+        <div id="feature"> <!--Feature unfeature list start-->
+            <h1> Feature/Unfeature Liste </h1> 
+            
+            <p class="tekst"> Herfra begyndte vi at udarbejde en feature / unfeature liste, som skulle hjælpe med at afgøre, hvad siden skulle indeholde og hvad den ikke skulle indeholde. Bagefter blev vi mere bevidste om, hvad der var vigtigt at siden skulle fokusere på, og hvilke problemer den skulle løse. </p> 
+            
+           <p class="tekst"> På baggrund af de erfaringer vi havde gjort os med brainstorm, user stories (personas) og feature/unfeature liste, var det nemmere for os at arbejde mere målrettet mod en løsning, der afspejlede de forventninger, Mejeri Niels, havde til sin hjemmeside. Vi lærte, at det både er tidsbesparende og økonomisk besparende, at gøre sig den umage. I løbet af sådan en udviklingsproces, kan der opstå mange uenigheder og forvirringer, derfor er det vigtigt at forventningsafstemme fra starten, så man er enig om, hvad der skal ske i løbet af processen. Jo længere man kommer i processen, jo dyrere bliver det at skifte kurs. Netop af den grund, er den en stor fordel at få gjort sig disse tanker og processer, inden man begynder at designe og udvikle en hjemmeside. </p> 
+            
+        </div>  <!--Feature unfeature list slut-->  
+            
+            
+        <div id="wireframes"> <!--Wireframes start-->
+            <h1> Wireframes</h1> 
+            
+           <p class="tekst"> Da vi havde været igennem den indledende proces (Empathize/Define fase), begyndte vi at skitsere, hvordan Det bette Ismejeris hjemmeside kunne se ud. Vi startede med at tegne nogle wireframes, som skulle give et overordnet billede på, hvordan siden kunne se ud. </p> 
+            
+           <p class="tekst"> Wireframes er et skitseværktøj, som fokuser på navigation, funktion og indhold på siden. Man har ikke fokus på udseende, design, fremtoning osv. Derfor brugte vi ikke tid på at indsætte billeder, tekst, logoer osv. Her koncentrer man sig om skelettet til hjemmesiden, derfor er det ikke vigtigt at den ser godt ud rent grafisk, det ødelægger tværtimod tanken omkring et wireframe, som er en grov skitse. </p> 
+            
+        </div>  <!--Wireframes slut--> 
+            
+            
+        <div id="wireframecontainer">
+            
+            <img id="wireframe" class="allebilleder" src="Billeder/wireframe.jpg">
+            
+            <blockquote> Wireframe fra Det Bette Ismejeri</blockquote>
+            
+        </div>
+            
+            <div id="felt6"> &nbsp;</div> <!--holder footer nede på siden-->
+        
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
