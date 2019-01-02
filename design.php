f669e4320a3ec149dd01a815540ea78e555b1014
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:07:22 +0100

    Update design.php

--- design.php
+++ design.php
@@ -12,58 +12,59 @@
         
         <div id="pagewrap">
         
-        <div id="illustrator"> <!--Illustrator box start-->
+        <a href="illustrator.php"><div id="illustrator"> <!--Illustrator box start-->
             <div id="itekst">
                 <h1> Illustrator</h1>
                 <p> Vectorgrafik, Artboards, logoer, Figurer, Layer Masks osv.</p>
             </div>
                 <div id="ibillede">    
-                    <img id="ibillede" class="allebilleder" src="Billeder/ibillede.jpg"> 
+                    <img id="ibillede" class="allebilleder" src="Billeder/ibillede400px.jpg"> 
                 </div>
-        </div> <!--Illustrator box slut-->
+        </div> </a><!--Illustrator box slut-->
             
-        <div id="ps"> <!--Photoshop box start-->
+        <a href="photoshop.php"><div id="ps"> <!--Photoshop box start-->
             <div id="pstekst">
                 <h1> Photoshop </h1> 
                 <p> Billedebehandling, Manipulation, Pixels, Artboards, Layer osv.</p>
             </div>
                 <div id="psbillede">    
-                    <img id="psbillede" class="allebilleder" src="Billeder/psbillede.jpg"> 
+                    <img id="psbillede" class="allebilleder" src="Billeder/psbillede400px.jpg"> 
                 </div>
-        </div> <!--Photoshop box slut-->    
+        </div></a> <!--Photoshop box slut-->    
         
-        <div id="fotografering"> <!--Fotografering box start-->
+        <a href="fotografering.php"><div id="fotografering"> <!--Fotografering box start-->
             <div id="fototekst">
                 <h2> Fotografering</h2>
                 <p> Lukketid, ISO, Blænde, lukkevide, Lys osv.</p>
             </div>
                 <div id="fotobillede">
-                    <img id="fotobillede" class="allebilleder" src="Billeder/fotobillede.jpg">
+                    <img id="fotobillede" class="allebilleder" src="Billeder/fotobillede400px.jpg">
                 </div>
-        </div> <!--Fotografering box slut-->
+        </div> </a><!--Fotografering box slut-->
     
-        <div id="gestalt"> <!--Gestaltlove box start-->
+        <a href="gestaltlove.php"><div id="gestalt"> <!--Gestaltlove box start-->
             <div id="gestalttekst">
                 <h2> Gestaltlove</h2>
                 <p> Proximity, Continuity, Simalarity, Perception osv.</p>
                 
             </div>
                 <div id="gestaltbillede">
-                    <img id="gestaltbillede" class="allebilleder" src="Billeder/gestaltbillede.jpg">
+                    <img id="gestaltbillede" class="allebilleder" src="Billeder/gestaltbillede400px.jpg">
                 </div>
-        </div>  <!--Gestaltlove box slut-->
+        </div> </a> <!--Gestaltlove box slut-->
         
-        <div id="farve"> <!--Farve og typografi box start-->
+        <a href="farve.php"><div id="farve"> <!--Farve og typografi box start-->
             <div id="farvetekst">
                 <h2> Farve og Typografi </h2>
                 <p> Farvehjul, Primær Farver, RGB, CMYK, Fonts, Weights, </p>
                 
             </div>
                 <div id="farvebillede">
-                    <img id="farvebillede" class="allebilleder" src="Billeder/farvebillede.jpg">
+                    <img id="farvebillede" class="allebilleder" src="Billeder/farvebillede400px.jpg">
                 </div>    
-        </div> <!--Farve og Typografi box slut-->
+        </div></a> <!--Farve og Typografi box slut-->
     
+             <div id="felt12"> &nbsp;</div> <!--holder footer nede på siden-->
             
         </div> <!--pagewrap slut-->
         
