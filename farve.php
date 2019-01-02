64e648c27554e04d838ac81de54c0b0a09fba078
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:07:28 +0100

    Create farve.php

---
+++ farve.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head> 
+        
+        <title>Farve og Typografi</title>
+        <?php include "overordnet.inc" ?>
+        <link rel="stylesheet" href="CSS/farve.css">
+
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+
+        <div id="indledningfarve"> <!--indledningfarve start-->
+            <h1>Farve og Typografi</h1> 
+            
+            <p class="tekst"> Vi finder farver og typografi uanset hvor vi kigger hen, alt lige fra mælkekartonen ned i Netto til forsiden af Bo Bedre.</p> 
+            
+            <p class="tekst">Den rigtige kombination mellem farver og typografi, kan være med til at skabe en stærk visuel identitet, som kan gøre dit brand eller virksomhed nemmere at genkende.</p> 
+            
+            <p class="tekst">Det er derfor vigtigt, at vi forstår, hvilken effekt farver og typografi har på kunder og forbrugere.</p> 
+              
+        </div>  <!--indledningfarve slut-->
+            
+            
+            
+            
+        <div id="typografi"> <!--typografi start-->
+            <h2> Typografi </h2> 
+            
+            <p class="tekst">Det er ikke kun skrifttypen. Det gælder også fonte, kolonner, læsbarhed, billeder, logo osv.</p>
+            
+            <p class="tekst">Typografi handler ligeså meget om det samlede visuelle indtryk. </p>
+            
+            <p class="tekst">Det er vores førstehåndsindtryk, når vi skimmer en artikel eller et website, der er afgørende for om vi gider at læse den. Vores underbevidsthed bemærker læsbarhed, skrifttyper, whitespace osv.</p>
+            
+            
+        </div>  <!--typografi slut-->
+            
+            
+            
+        <div id="typeface"> <!--typeface  start-->
+            <h2> Fonts og Weigths udgør tilsammen et typeface </h2> 
+            
+            <p class="tekst"> Et eksempel kunne være Arial, Helvetica osv. Det er kun skrifttyper – ikke typefaces. 
+            Alt omkring fonte drejer sig om skrifttype.</p>
+            
+            <p class="tekst">Når man kombinerer fonte med Weights giver det et typeface. 
+            Eksempel kunne være Arial (font) Regular (Weight) – Andre weights kunne være italic, bold, bold italic, Arial Black.</p>
+            
+            <p class="tekst">Her vil din typeface være Arial Regular.</p>
+            
+        </div>  <!--typeface slut-->    
+            
+            
+            
+            
+            
+        <div id="rettigheder"> <!--rettigheder start-->
+            <h2> Husk rettigheder</h2> 
+            
+            <p class="tekst"> Man skal have rettigheder til at bruge fonte/typefaces, da mange skrifttyper/fonts har copyright.</p>
+            
+            <p class="tekst"> Ofte ejet af store firmaer som Coca Cola – Derfor skal man lige tænke sig om inden man bruger Coca Colas typeface på det lokale old boys holds hjemmeside.</p>
+            
+            <p class="tekst"> Omvendt findes der mange forskellige gratis typefaces eks. Google fonts, What the font, 1001 fonts osv.</p>
+            
+            <p class="tekst"> I forbindelse med udarbejdelsen af denne hjemmeside har jeg brugt Google Fonts. Her har jeg brugt Oswald fonten til mine overskrifter, da det er en sans serif, dvs den har ingen fødder. Sans seriffer bruges ofte til overskrifter.</p>
+            
+            <p class="tekst">Til min brødtekst har jeg valgt skriftypen Halvant, som er en serif. Det vil sige den har fødder, hvilket gør den letlæselig, og man bliver ikke træt i hovedet som læser. </p>
+            
+        <div id="typefacecontainer">
+            
+                <img id="typebillede" class="allebilleder" src="Billeder/typebillede.jpg">
+                
+                <blockquote>Model til at kombinere fonts og weights ↑</blockquote>            
+            </div>     
+            
+        </div>  <!--rettigheder slut-->  
+            
+            
+            
+            
+        <div id="farvelare"> <!--Farvelære start-->
+            <h2> Farvelære </h2> 
+            
+            <p class="tekst"> Når man arbejder med farvelære er det vigtigt at være bevidst om den psykologi, som farver har på os mennesker.</p>
+            
+            <p class="tekst"> Farvers betydning og symbolik varierer fra kultur til kultur.</p>
+            
+            <p class="tekst">Man kan altså ikke skrive en universal farveguide til hele verden, der vil altid være forskelle på kulturer, og dermed hvilken betydning vi tilægger de enkelte farver</p>
+            
+            <p class="tekst">Et studie udført blandt Himba-folket i Namibia, viste at de ikke er i stand til at genkende farven blå. De blev præsenteret et billede med grønne firkanter, og en enkelt blå firkant, som de ikke var i stand til at udpege. </p>
+            
+            <p class="tekst">Hvorimod de bagefter blev præsenteret for et billede med grønne firkanter, hvor en af firkanterne havde en anden nuance af grøn. Den kunne de allesammen udpege. </p>
+            
+            <p class="tekst">Forskerne tror, at det er kulturelt betinget. Der findes for eksempel ikke et ord for ‘blå’ i Himba-folkets sprog. Derimod har de flere ord for grøn. De kan selvsagt se farven, men lægger sansynligvis ikke mærke til den, funderer forskerne.(videnskab.dk/sporg-videnskaben/hvordan-pavirker-farver-os)</p>
+            
+            <p class="tekst">En forsker ved navn Zena O´ Conner, mener at vores opfattelse af farver påvirkes af forskellige individulle faktorer. Hun mener, at der findes meget få farver, som har en universel effekt. "Forholdet mellem reaktion og farve er meget kompliceret og kan være påvirket af alt fra alder og køn til hukommelse og kultur," (videnskab.dk/sporg-videnskaben/hvordan-pavirker-farver-os)</p>
+        
+        </div>  <!--Farvelære slut-->  
+            
+            
+           <div id="farvehjulet"> <!--Farvehjulet start-->
+            <h2> Farvehjulets Opbygning </h2> 
+               
+            <p class="tekst">Det her farvehjul har jeg selv tegnet i Photoshop. Det gjorde vi den dag, vi havde og om Typografi og Farvelære med Carsten.</p>            
+               
+            <h2> Primærfarver </h2> 
+               
+            <p class="tekst"> Primærfarver er de rene farver Rød, Gul og Blå (RGB). 
+            RGB er addidativ det lægger farver til.</p>
+               
+               
+            <h2> Sekundærfarver </h2> 
+            
+            <p class="tekst"> Sekundærfarver er blandet af to primær farver.</p> 
+               
+            <h2> Tertiære farver </h2> 
+            
+            <p class="tekst"> Tertiære farver er blandet af sekundær og primær farver. Et farvesystem er ikke absolut, da det kan variere afhængigt af mennesket og kulturen.</p>  
+               
+        <div id="farvehjulcontainer"> 
+            
+                <img id="farvehjul" class="allebilleder" src="Billeder/farvehjul.jpg">
+                           
+                <blockquote>Farvehjulet</blockquote>
+        </div>      
+               
+            <p class="tekst"> Farvehjulet stammer helt tilbage fra 1600-tallet. Det var Issac Newton som første gang demonstrede, hvordan lyset brydes igennem en prisme</p>   
+               
+            <p class="tekst">Han nåede til den konklusion at farverne gul, rød og blå var primær farver, da disse farver ikke kunne skabes ved at blande andre farver. De blev placeret i den inderste trekant i centrum af farvehjulet. </p>
+               
+            <p class="tekst">Sekundærfarver opstod ved at blande to primærfarver f.eks. rød og blå som giver farven lilla. De andre sekundærfarver er gul (grøn og rød) og lyseblå (blå og grøn). </p>   
+            
+            <p class="tekst">De tertiære farver blev placeret yderst i farvecirklen. Det er farver som er blandet af en primærfarve og en sekundærfarve f.eks. orange som er blandet af rød(primær) og gul(sekundær)</p>   
+               
+            <p class="tekst"> De farver som ligger overfor hinanden kaldes for komplementære farver. Det kunne f.eks. være blå og gul. Det skaber en optisk kontrast, som ofte kan findes i naturen.</p>
+               
+               
+               
+            
+        </div>  <!--Farvehjulet slut-->     
+            
+            
+        <div id="farverum"> <!--Farverum start-->
+            <h2>Farverum</h2> 
+            
+            <h2>CMYK</h2>
+            
+            <p class="tekst"> Den er substraktiv – DVS. den trækker farver ud af RGB. Hvis du blander de tre primærfarver i cmyk får man farven sort. 
+            Den er beregnet til tryk.</p>
+            
+             
+        <div id="cmykcontainer"> 
+            
+                <img id="cmyk" class="allebilleder" src="Billeder/cmyk.jpg">
+                           
+                <blockquote>CMYK</blockquote>
+        </div>     
+            
+            <h2>RGB</h2>
+            
+            <p class="tekst"> Hvis man blander de tre primærfarver, med maximum værdi (255) så får man farven hvid. 
+            RGB bruges til skærme.</p>
+            
+         <div id="rgbcontainer"> 
+            
+                <img id="rgb" class="allebilleder" src="Billeder/rgb.jpg">
+                           
+                <blockquote>RGB</blockquote>
+        </div>     
+            
+        
+        </div>  <!--Farverum slut-->      
+            
+            
+            
+        <div id="kulor"> <!--Kulør start-->
+            <h2>Kulør, Mæthed og Lyshed</h2> 
+            
+            <p class="tekst"> Kulør er en variation af en farve, eller en graduering af en farve. </p>
+            
+            <p class="tekst"> Mæthed: Farven blandes med forskellige mængder af gråtoner. </p>
+            
+            <p class="tekst"> Lyshed: En kulør blandes med ren sort eller hvid.</p>
+            
+            <p class="tekst"> Vi har bl.a. brugt color-pickeren til at bestemme den rigtige Hex-kode. Man kan også justere på både hue, saturation, brightness indtil man finder den farve, man søger efter.</p>
+            
+         <div id="farvepalettecontainer"> 
+            
+                <img id="farvepalette" class="allebilleder" src="Billeder/farvepalette.jpg">
+                           
+                <blockquote>I Photoshop har vi arbejdet med Kulør, Mæthed og Lyshed</blockquote>
+        </div>         
+        
+        </div>  <!--Kulør farver-->     
+            
+            
+        
+        <div id="farvesammen"> <!--Farvesammensætning start-->
+            <h2>Farvesammensætning</h2> 
+            
+            <p class="tekst"> -	Monokrom er en kulør som justeres ifh. til lyshed og mætning.</p>
+            
+            <p class="tekst"> -	Analoge sammensætninger: En kulør indenfor et bredere spektrum af lyshed og mætning.</p>
+            
+            <p class="tekst"> -	Komplementær: Farver der ligger direkte overfor hinanden i farvecirklen.</p>
+            
+             <p class="tekst"> - Split komplementær: Farver der ligger direkte overfor hinanden, med et lidt bredere farvespektrum.</p>
+            
+             <p class="tekst"> - Triader: Tre farver som ligger lige langt fra hinanden. (ligner et peace tegn).</p>
+            
+             <p class="tekst"> - Tetriader: dobbelt komplementære – Fire farver som er komplementære.</p>
+            
+        <div id="farvesamcontainer"> 
+            
+                <img id="farvesam" class="allebilleder" src="Billeder/farvesam.jpg">
+                           
+                <blockquote>Denne model kan bruges til at se forskellige farvesammensætninger</blockquote>
+        </div>  
+            
+            <p class="tekst"> Vi fik til opgave at tegne 11 spøgelser, hvor vi skulle bruge forskellige typografier og farver til at beskrive stemningerne ond, venlig, vigtig, stærk, blød, maskulin, feminin, barnlig, streng, alvorlig og kedelig</p>
+            
+        <div id="stemningercontainer"> 
+            
+                <img id="stemninger" class="allebilleder" src="Billeder/stemninger.jpg">
+                           
+                <blockquote>De 11 forskellige spøgelser jeg tegnede i Photoshop</blockquote>
+        </div>  
+            
+            <p class="tekst"> Til f.eks. det onde spøgelse valgte jeg farven sort, som kan symbolisere forskellige ting bl.a. eksklusivitet, rigdom, elegance osv. Men det kan også symbolisere noget ondt, skræmmende, mørke osv. Derfor synes jeg, at farven passede godt til det "onde spøgelse"</p>
+            
+         <div id="ghostondcontainer"> 
+            
+                <img id="ghostond" class="allebilleder" src="Billeder/ghostond.jpg">
+                           
+                <blockquote>Det "onde spøgelse"</blockquote>
+        </div>     
+            
+            
+            
+        
+        </div>  <!--Farvesammensætning farver-->      
+            
+            
+            
+            
+            
+            
+            <div id="felt23"> &nbsp;</div> <!--holder footer nede på siden-->     
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
