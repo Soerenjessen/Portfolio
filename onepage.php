049adab49bdaaace24240026fadfdbbebd5996f1
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:08:18 +0100

    Create onepage.php

---
+++ onepage.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head> 
+        
+        <title> One Page-projekt</title>
+        <?php include "overordnet.inc" ?>
+        <link rel="stylesheet" href="CSS/onepage.css">
+
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+
+        <div id="oneintro"> <!--One Page indledning start-->
+            <h1> One Page-Projekt </h1> 
+            
+            <p class="tekst"> Til One Page-projektet brugte vi mange af metoderne og tilgangene fra Design Thinking, derfor anbefaler jeg, at du læser hvad jeg har skrevet om Design Thinking først. 
+            Se <a href="designthinking.php">her</a> </p> 
+            
+            <p class="tekst"> I forbindelse med vores One Page-projekt skulle jeg arbejde sammen med Gert, hvilket gik super godt. Jeg synes, at vi fik udarbejdet et solidt projekt, hvor vi var blevet tilpas udfordret, og vi følte begge at vi havde flyttet os rent fagligt. </p> 
+              
+            <p class="tekst">Hele projektet forløb henover 2,5 uge, hvor vi skulle igennem de forskellige faser i en design thinking proces. Vi startede med researchfasen, som indeholdte empathize og define. Dernæst gik vi over i designfasen, som bød på ideate og prototyping. Til slut beskæftigede vi os med produktionsfasen, hvor vi arbejde med kode, tekstforfatning og implementering.</p> 
+            
+        </div>  <!--One Page indledning slut-->
+            
+             <div id="forsidebiotexcontainer"> <!--Forside på biotex start-->
+            
+                <img id="forsidebiotex" class="allebilleder" src="Billeder/forsidebiotex.jpg">
+                
+                <blockquote> Sådan så forsiden ud på vores One Page ↑</blockquote> 
+                        
+            </div> <!--Forside på biotex slut-->
+            
+            
+        <div id="researchfasen"> <!--Researchfasen start-->
+            <h2> Empathize Part 1</h2> 
+            
+            <p class="tekst"> Vi startede mandag morgen med at skulle trække et tilfældigt produkt op af en indkøbspose fra Bilka, som Niels havde med. Skæbnen ville at Gert og jeg skulle trække Biotex Ultimate Color vaskemiddel, og herfra gik det stærkt. </p> 
+            
+            <p class="tekst"> Vi startede dagen ud med en Brainstorm, omkring alle vores ideer med Biotex og vaskemiddel generelt. Her arbejdede vi både divergent og konvergent med vores brainstormproces</p> 
+            
+            <div id="research1container">
+            
+                <img id="research1" class="allebilleder" src="Billeder/research1.jpg">
+                
+                <blockquote> Brainstorm omkring Biotex ↑</blockquote> 
+                        
+            </div> 
+            
+            <p class="tekst"> Vi ønskede et overblik over vores brainstormproces, derfor inddelte vi vores ideer fra brainstormprocessen i forskellige kategorier.</p> 
+            
+            
+            <div id="research2container">
+            
+                <img id="research2" class="allebilleder" src="Billeder/research2.jpg">
+                
+                <blockquote> Kategorisering af Brainstorm ↑</blockquote> 
+                        
+            </div> 
+            
+            <p class="tekst"> Efter flere omganges brainstorming, besluttede vi os for at vi ville ud op observere, hvordan brugerne interagerede med Biotex Ultimate Color, derfor tog vi ud i forskellige supermarkeder og betragtede, hvordan Biotex tog sig ud på hylderne. Samtidig foretog vi nogle semistrukturerede interviews med butiksmedarbejderne i bl.a. Fakta og Føtex. 
+            Dette gav rigtigt meget godt brugbart data, som vi brugte videre i forløbet.</p>
+            
+            <div id="research3container">
+            
+                <img id="research3" class="allebilleder" src="Billeder/research3.jpg">
+                
+                <blockquote> Observation i Føtex ↑</blockquote> 
+                        
+            </div> 
+            
+            <p class="tekst">
+            Dagen efter gik vi i gang med at formulere nogle spørgsmål, som vi havde undret os over. Dernæst foretog vi nogle interviews som var semistruktureret. Hvilket gav en masse data, som bidrog til en bredere forståelse af folks opfattelse af produktet.  
+            </p>
+            
+             <div id="research4container">
+            
+                <img id="research4" class="allebilleder" src="Billeder/research4.jpg">
+                
+                <blockquote> Interview med brugere ↑</blockquote> 
+                        
+            </div> 
+            
+            
+        </div>  <!--Researchfasen slut-->
+    
+            
+        <div id="researchfasen2"> <!--Researchfasen 2 start-->
+            <h2> Empathize Part 2</h2> 
+            
+             <p class="tekst"> Efter vores to interviewrunder og observationer tirsdag formiddag, begyndte vi at strukturere og analysere meget af den data, vi havde indsamlet.</p>
+            
+            
+        <div id="research5container">
+            
+                <img id="research5" class="allebilleder" src="Billeder/research5.jpg">
+                
+                <blockquote> Strukturering af data ↑</blockquote> 
+                        
+        </div> 
+            
+            <h2>User Stories</h2>
+            
+            <p class="tekst"> Vi havde defineret at den målgruppe vi ville appellere til, var unge mennesker i alderen 20-35 år. Derfra begyndte vi at definere nogle user stories, som skulle hjælpe os med at tydeliggøre hvad vores One-page side burde indeholde i forhold til vores målgruppe. De tre user stories som vi endte med at bruge kan du læse 
+            <a href="User1.pdf">Her</a></p> 
+            
+            <h2> Feature / Unfeature Liste</h2> 
+            
+            <p class="tekst"> Vi brugte tid på at formulere en Feature/Unfeature liste, som skulle hjælpe os med at definere, hvad vores side skulle indeholde, og hvad den ikke skulle indeholde.</p>
+            
+           <div id="featurecontainer">
+            
+                <img id="featue" class="allebilleder" src="Billeder/feature.jpg">
+                
+                <blockquote> Feature / Unfeature Liste ↑</blockquote> 
+                        
+            </div> 
+            
+            
+            <h2>Indholdshierarki</h2>
+            
+            <p class="tekst"> Til sidst udarbejdede vi et indholdshierarki, som havde til formål at hjælpe os med at definere, hvilken rækkefælge og orden vores elementer på siden skulle rangeres og præsenteres efter. Måden vi gjorde dette på, var ved at bruge farvekoder på vores elementer. Inddelingen i farvekoder var med til at give os et meget bedre overblik over vores side.</p>
+            
+            <div id="farvekodecontainer">
+            
+                <img id="farvekode" class="allebilleder" src="Billeder/farvekode.jpg">
+                
+                <blockquote> Farvekode ↑</blockquote> 
+                        
+            </div> 
+
+            
+        </div>  <!--Researchfasen 2 slut-->    
+            
+            
+        <div id="designfasen"> <!--Designfasen start-->
+            <h2> Designfasen</h2> 
+            
+            <p class="tekst"> På baggrund af den viden og data vi havde indsamlet i research-fasen, var vi nu parate til at begynde på designfasen, hvor både Ideate og Prototype var en del af processen. </p> 
+        
+            
+            <p class="tekst"> Det første vi startede med, var at definere hvilken type side vi ville skabe. Her nåede vi frem til den konklusion, at vi ville udarbejde en informationsside til Biotex Ultimate Color.</p> 
+                
+            <p class="tekst"> Vi startede dagen med en klassisk brainstorm, hvor vi skulle prøve at genere nogle ideer til, hvordan vi kunne forklare, informere og kommunikere på en anderledes måde.</p>
+                
+        
+        <div id="brainycontainer">
+            
+                <img id="brainy" class="allebilleder" src="Billeder/brainy.jpg">
+                
+                <blockquote> Brainstorm ↑</blockquote> 
+                        
+        </div>         
+                
+                
+            <p class="tekst"> Det var i den forbindelse at der dukkede en sjov figur op ved navn Biotex Bent. Tanken bag Biotex Bent var at skabe en figur, som folk ville forbinde med Biotex Ultimate Color – Lidt ligesom Normals figur, Michelin manden, Peter fra Leasy osv. Biotex Bent blev en figur som vi brugte til at kommunikere til målgruppen. Vi blev grebet af ideen om at skabe en karakter til Biotex, og bygge et univers op omkring Biotex Bent. Bents rolle på siden, var at kommunikere i små sjove statements, samt fortælle historier og anekdoter om sit eget liv med Biotex. </p>     
+                
+            
+           <div id="biotexbentcontainer">
+            
+                <img id="biotexbent" class="allebilleder" src="Billeder/biotexbent.jpg">
+                
+                <blockquote> Biotex Bent ↑</blockquote> 
+                        
+            </div> 
+        
+            
+            <p class="tekst"> Derudover tegnede vi nogle wireframes, som skulle vise siden i forskellige skærmstørrelser. Her arbejdede vi ud fra princippet ”mobile first” – Efter vi havde styr på mobilversion, gik vi i gang med at udvikle til tablet og desktop. Se alle vores wireframes 
+            <a href="hierarki.pdf">her</a></p>
+            
+            <div id="wireframecontainer">
+            
+                <img id="wireframe" class="allebilleder" src="Billeder/wireframes.jpg">
+                
+                <blockquote> Wireframe til Mobil ↑</blockquote> 
+                        
+            </div> 
+            
+            
+            
+            <p class="tekst">Til sidst i designfasen skulle vi skabe et moodboard. Vores moodboard blev meget abstrakt i forhold til farver, former, stemninger osv. På det tidspunkt vi kreerede den, havde vi været meget langt omkring med vores ideer. Vi havde arbejdet med alt lige fra Biotex Blues, til Michelin Manden, til cafebesøg. Vores moodboard kunne godt have været mere konkret, så den havde været nemmere at forstå for udefrakommende.</p>
+            
+            <div id="moodboardcontainer">
+            
+                <img id="moodboard" class="allebilleder" src="Billeder/moodboard.jpg">
+                
+                <blockquote> Moodboard ↑</blockquote> 
+                        
+            </div> 
+
+            
+        </div>  <!--Designfasen slut-->  
+            
+            
+            
+            
+        <div id="produktionsfasen"> <!--Produktionsfasen start-->
+            <h2> Produktionsfasen</h2> 
+            
+            <p class="tekst"> Efter at vi var færdige med designfasen gik vi over i produktionsfasen. Det var her at vi skulle til at kode vores One Page. Her arbejdede i teksteditoren Brackets, hvor vi kodede i HTML, CSS og CSS grids.</p> 
+        
+            
+            <p class="tekst"> Dette var den første side vi har kodet i CSS grid. Af den grund, havde vi også nogle udfordringer undervejs rent kodemæssigt. Vi brugte meget tid på at få vores mobilversion til at fungere 100%, men da den først fungerede, gik det hurtigt med at udvikle tablet og desktop</p> 
+            
+            <h2>Webtekster</h2>
+                
+            <p class="tekst"> Det var også i produktionsfasen, at vi skrev alle vores webtekster til siden. Her var vi meget bevidste om, at vi ville kommunikere i et letforståeligt og humoristisk sprog.  Du kan læse webteksterne 
+            <a href="webtext.pdf">her</a></p>
+            
+            
+        <div id="biotekstcontainer">
+            
+                <img id="biotekst" class="allebilleder" src="Billeder/biotekst.jpg">
+                
+                <blockquote> Teksteksempel ↑</blockquote> 
+                        
+        </div>         
+               
+            
+            <h2>SEO</h2>
+            
+            <p class="tekst"> I forhold til SEO, gjorde vi os flere overvejelser undervejs bl.a. prøvede vi at minimere vores billedstørrelser for at reducere loadhastigheden. Derudover brugte vi tid på at skrive alternative billedtekster til vores billeder.</p> 
+                
+        
+        <div id="billedtekstercontainer">
+            
+                <img id="billedtekster" class="allebilleder" src="Billeder/billedtekster.jpg">
+                
+                <blockquote> Vi har arbejdet med Alt-tekst ↑</blockquote> 
+                        
+        </div>         
+                
+            
+            <h2>W3 Validator</h2>
+                
+            <p class="tekst"> Vi fik vores HTML og CSS-kode valideret inde på W3 Schools validator. Den gjorde os opmærksom på nogle kodefejl, som vi fik rettet inden afleveringsfristen.</p>     
+                
+            
+           <div id="validatorcontainer">
+            
+                <img id="validator" class="allebilleder" src="Billeder/validator.jpg">
+                
+                <blockquote> Vi brugte W3 Schools validator ↑</blockquote> 
+                        
+            </div> 
+        
+            <h2>Github</h2>
+            
+            <p class="tekst"> Herefter oprettede vi et repository inde på Github, hvor vi oploadede alt vores kode, grafik og øvrige filer.</p>
+            
+            
+            <div id="githubcontainer">
+            
+                <img id="github" class="allebilleder" src="Billeder/github.jpg">
+                
+                <blockquote> Mit Repository inde på Github ↑</blockquote> 
+                        
+            </div> 
+            
+            
+            <h2> Refleksion omkring One Page-Projektet</h2>
+            
+            <p class="tekst">One Page-projektet er et af de projekter på 1. semester, hvor jeg synes, at jeg har lært mest, og flyttet mig mest. Selve længden på omkring 20 dage, gjorde at jeg kom meget dybere ned i de enkelte processer, hvilket var enormt lærerigt.</p> 
+            
+            <p class="tekst">I forbindelse med One Page-projektet lærte jeg en del, men en af de punkter, hvor jeg lærte mest var CSS grids. Vi havde ikke arbejdet specielt meget med CSS Grids forinden, derfor var det også naturligt nok, at vi ville begå nogle fejl undervejs i kodefasen</p> 
+                
+            <p class="tekst">En af de fejl vi begik, var at vores navngivning af billeder, id´er og classes, de var alt for forvirrende. Det gjorde det svære overskue vores kode, jo længere vi kom. </p> 
+            
+            <p class="tekst">Det har været en af de ting, som jeg bl.a. har været meget opmærksom på i forhold til denne her side. Bruge tydelige og forklarende navne som id, class, selector osv. Det gør det meget nemmere, når man kommer længere i processen.</p> 
+            
+            <p class="tekst">Derudover var det spændende at arbejde med en Design Thinking proces over en længere periode, her havde vi meget bedre mulighed for at komme i dybden med de forskellige faser, især Empathize og Define fasen.</p> 
+            
+         <div id="ctacontainer">
+            
+                <img id="cta" class="allebilleder" src="Billeder/cta.jpg">
+                
+                <blockquote>Call to action fra Biotex Bent↑</blockquote> 
+                        
+        </div>     
+            
+            
+        </div>  <!--Produktionsfasen slut-->     
+            
+        
+            <div id="felt25"> &nbsp;</div> <!--holder footer nede på siden-->     
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
