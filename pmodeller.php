745282bc91651783db5ff9bebf535d0111633294
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:08:31 +0100

    Create pmodeller.php

---
+++ pmodeller.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head> 
+        
+        <title> Kommunikation og Produktionsmodeller</title>
+        <?php include "overordnet.inc" ?>
+        <link rel="stylesheet" href="CSS/pmodeller.css">
+
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+
+        <div id="hvad"> <!--Hvad er kommunikation start-->
+            <h1> Hvad er kommunikation?</h1> 
+            
+            <p class="tekst"> Kommunikation er det sprog som binder de forskellige fag sammen. Vi kommunikerer i forskellige sammenhænge på multimediedesigneruddannelsen, uanset om vi laver webtekster til hjemmesider, koder i Brackets, designer logoer osv. Vi kommunikerer i alt hvad vi foretager os.</p> 
+            
+           <p class="tekst"> Vi kan som mennesker og studerende ikke undgå at kommunikere, og vi gør det på mange forskellige måder f.eks. igennem vores tøjvalg, kropssprog, stemmeføring osv. Men vi kommunikerer også i høj grad, omkring de ting vi ikke siger, og ikke gør. Det er vigtigt at huske, at vi oftest kommunikerer igennem sprog mennesker i mellem, men vi skal være mindst ligeså bevidste om vores non-verbale kommunikation.</p> 
+              
+            <p class="tekst">Overordnet set har vi tre grene af kommunikation: 
+            </p> 
+            
+            <p class="tekst">
+            -	Den verbale kommunikation: Det talte sprog imellem mennesker. -	  Den non-verbale kommunikation: Det er vores kropssprog. Den måde vi kommunikerer med vores krop, altså vores gestik og mimik. Det kan også være billeder osv. 
+            -	Skriftlig kommunikation: Den måde vi kommunikerer på skrift. 
+            </p>  
+            
+            <h2>Vi fokuserer på hvem kommunikerer vi til?</h2>
+            
+            <p class="tekst">Vi har en afsender, dernæst en modtager, og til sidst et budskab. I sidste ende er det vigtigt at have fokus på perception, hvilket er den måde vi forstår et budskab på, ud fra vores kultur, opdragelse og verdenssyn. 
+            </p>  
+                    
+        </div>  <!--Hvad er kommunikation slut-->
+            
+            
+        <div id="kongruent"> <!--Kongruent start-->
+            <h2> Kongruent og inkongruent</h2> 
+            
+            <p class="tekst"> Kongruent kommunikation handler om den sammenhæng imellem de forskellige typer af kommunikation. 
+            Et eksempel kunne være imellem vores verbale og nonverbale kommunikation. Det vi siger, er også det vi viser med vores kropssprog. Dette er med til at skabe en tryghed ved modtageren, da der er klarhed om hvad der menes.  Verbal ”Du spiller fedt guitar” non-verbal ”man vipper i takt med højre fod, og headbanger” – Der er sammenhæng imellem din udtalelse, og det dit kropssprog viser. 
+            </p> 
+            
+            <p class="tekst"> Inkongruent kommunikation er når der ikke er sammenhæng imellem de forskellige kommunikationstyper. Dette er med til at skabe en usikkerhed og utryghed for modtageren. 
+            Et eksempel kunne være at din verbale kommunikation er modsatrettet i forhold til din non-verbale kommunikation. 
+            </p> 
+            
+            <p class="tekst"> Ex: Verbal ”Du spiller godt guitar” Non-verbal ”Man holder sig for ørene, og skærer tænder”
+            Der er altså ikke sammenhæng imellem det du siger, og det du viser. </p> 
+            
+            
+        </div>  <!--Kongruent slut-->
+            
+        <div id="synkron"> <!--Synkron og Asynkron kommunikation start-->
+            <h2> Synkron og Asynkron kommunikation handler om kommunikation i forhold til tid. </h2> 
+            
+            <p class="tekst"> -	Synkron kommunikation, er når de              kommunikerende enten skriver eller taler på samme tid. Et eksempel kunne være en dialog, hvor man sidder fysisk over for hinanden. 
+            Eller en chat, hvor begge er online samtidig.  
+            </p> 
+                
+            <p class="tekst">-	Asynkron kommunikation betyder, at der er en tidsforskydning i forhold til kommunikationen. Det kunne f.eks. være et gammeldags Postkort, som typisk er flere dage undervejs. </p>  
+            
+        </div>  <!--Synkron og Asynkron kommunikation slut-->
+            
+        <div id="grundantagelser"> <!--Grundantagelser start-->
+            <h1> 5 grundantagelser om kommunikation </h1> 
+            
+            <p class="tekst"> 1. Man kan ikke undgå at kommunikere. Alt hvad vi gør, er kommunikation.</p> 
+            
+            <p class="tekst"> 2. Enhver kommunikation består af to dele. Selve  indholdet og oplysninger om det indbyrdes forhold, der er mellem   dem, der kommunikerer(Daugaard, 2018, S.9-10 ,Kommunikation MMD1 11092018 kommunikationsteori). 
+            Indholdet kan forstås på forskellige måder. Det handler om, hvordan vi perciperer hinanden. Hvor godt kender vi hinanden, og hvad er vores indbyrdes forhold.
+            </p> 
+                
+            <p class="tekst">3.	Den måde, vi udtrykker os på, og den måde, vi tolker hinandens kommunikation på, afhænger også af det forhold vi har til hinanden.(Daugaard, 2018, S.9-10 ,Kommunikation MMD1 11092018 kommunikationsteori).    
+            Den handler om den måde vi kommunikerer på i forhold til pauser og betoning.
+            </p>  
+            
+            <p class="tekst">4.	Mennesker kommunikerer både med en digital kode og en analog kode.(Daugaard, 2018, S.9-10 ,Kommunikation MMD1 11092018 kommunikationsteori).  
+            Digital kode: hvad der siges (ord)
+            Analog kode: hvordan det siges (tonefald, kropsprog, skrifttype/opsætning).
+            </p>
+            
+            <p class="tekst">5.	Kommunikationsudveksling er enten symmetrisk eller asymmetrisk afhængigt af, om den hviler på lighed eller forskel i magt. 
+            Når man kommunikerer symmetrisk, kommunikerer man på samme niveau. 
+            Ex: To hjertelæger har en dialog om kardiologi. 
+            Når man kommunikerer asymmetrisk kommunikere man ikke på samme niveau. Ex forældre taler til sit barn. 
+            </p>
+            
+        </div>  <!--Grundantagelser slut-->    
+            
+        <div id="paradigmer"> <!--Paradigmer start-->
+            <h2>Paradigmer</h2> 
+            
+            <p class="tekst"> Et paradigme er en måde at se verden på.(Daugaard, 2018, S.9-10 ,Kommunikation MMD1 11092018 kommunikationsteori).  
+            Her har vi arbejdet med to forskellige slags paradigmer. Det samfundsvidenskabelige paradigme og det humanistiske paradigme. 
+            </p> 
+            
+            <h2>Samfundsvidenskabelige paradigme</h2>
+            
+            <p class="tekst"> I det samfundsmæssige paradigme har vi et særligt fokus på afsenderen. Afsenderen ses som den styrende i kommunikationsprocessen. 
+            Under det samfundsvidenskabelige paradigme har vi arbejdet med nogle forskellige metoder bl.a. Laswells kanyleform og AIDA-modellen. 
+            </p> 
+                
+            <p class="tekst">Karaktertræk ved det samfundsvidenskabelige paradigme: 
+            </p>  
+            
+            <p class="tekst">
+                -	Objektivitet<br> 
+                -	Forklaring<br>
+                -	Kvantitativ tilgang<br>
+                -	Deduktion<br>
+                -	Stabile omgivelser<br>
+                -	Afsenderorienteret<br> 
+                -	Transmission<br>
+                -	Handlingsanvisende<br>
+                -	Modtageren ses som passiv (Busch, 2015, S-77, Kommunikation i multimediedesign)
+            </p>
+            
+            <h2> Det humanistiske paradigme</h2>
+            
+            <p class="tekst">Her er der fokus på modtageren, som ses som værende aktiv og medbestemmende. Der fokuseres primært på de humanistiske fagområder såsom sprog, kultur, kunst, psykologi, læring, organisation, antropologi, informationsteknologi osv. 
+            </p>
+            
+            <p class="tekst">Det humanistiske paradigme bliver også kaldt interaktionsparadigmet, netop fordi der forekommer interaktion imellem modtager og afsender. Her er selve den menneskelige interaktion mere interessant for kommunikationen end budskabet i sig selv.
+            </p>
+            
+            <p class="tekst">De undersøgelser man foretager sig i det humanistiske paradigme, beror på kvalitative undersøgelser. Man fokuser på hvad modtageren forstår ved et givent budskab, og hvilken subjektiv oplevelse de har. 
+            </p>
+            
+            <p class="tekst">Man anvender undersøgelsesmetoder, som sikrer en dybere forståelse for hvad brugeren oplever. Ud fra disse kvalitative undersøgelser anvender man en induktiv metode, da man generaliserer ud fra konkrete tilfælde. 
+            </p>
+            
+            <p class="tekst">Man undersøger det unikke frem for det generelle. Hvorefter man udleder overordnet antagelser på bagrund af få, men dybe undersøgelser.  
+            </p>
+            
+            <p class="tekst">Her ser man bort fra, hvilken intentionen afsenderen har haft. Her er det modtagerens oplevelse af budskabet der er det væsentlige.
+            </p>
+            
+            <p class="tekst">Karaktertræk i det humanistiske paradigme:</p>  
+            
+            <p class="tekst">
+                -	Subjektivitet<br> 
+                -	Fortolkning<br> 
+                -	Kvalitativ tilgang<br>
+                -	Induktion<br>
+                -	Situationsbestemt<br> 
+                -	Modtagerorienteret<br> 
+                -	Interaktion mellem mennesker<br>
+                -	Mennesket ses som aktivt<br>     
+                (Busch, 2015, S-68, Kommunikation i multimediedesign)
+            </p>
+            
+        </div>  <!--Paradigmer slut-->     
+            
+        <div id="aida"> <!--AIDA start-->
+            <h2> AIDA Modellen</h2> 
+            
+            <p class="tekst"> AIDA-modellen er et værktøj, som vi bruger til at forudsige effekten af en kommunikation. 
+            Når du har indsamlet kvantitative data, kan dette sætte ind i AIDA-modellen, og give et billede af hvordan man kan kommunikere i forhold til at skabe:  
+            </p> 
+            
+            <p class="tekst"> Attention (Brand Awarness).  
+            Bevidsthed omkring et produkt. 
+            </p> 
+              
+            <p class="tekst">Interest of the customer.
+            Skabe en interesse for produktet hos kunden.</p> 
+            
+            <p class="tekst">
+            Desire.
+            Kunden skal føle et behov for at eje produktet. Produktet skal går fra at være ”nice to have” til ”need to have”</p>  
+            
+            <p class="tekst">Action (Call to action)
+            Få kunden til at skride til handling, og enten købe produktet.</p> 
+                    
+        </div>  <!--AIDA slut-->
+            
+        <div id="laswell"> <!--Laswell start-->
+            <h2> Laswell Modellen</h2> 
+            
+            <p class="tekst"> I løbet af semesteret brugte vi Laswells model til at analysere en kampagnefilm. Se nedenstående eksempel. Filmen handler om respekt ved at stoppe med at drikke. 
+            </p> 
+            
+            <div id="laswellcontainer">
+            
+                <a href="Respektkampagne.pdf"><img id="laswell" class="allebilleder" src="Billeder/laswell.jpg"></a>
+                
+                <blockquote> Læs hele rapporten her - Klik på billedet ↑ </blockquote> 
+                        
+            </div>               
+        </div>  <!--Laswell slut-->    
+        
+            <div id="felt13"> &nbsp;</div> <!--holder footer nede på siden-->     
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
