3e5eadbd056854f4df00b01b8aaedd26bba2bf24
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:08:29 +0100

    Create photoshop.php

---
+++ photoshop.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head> 
+        
+        <title>Photoshop</title>
+        <?php include "overordnet.inc" ?>
+        <link rel="stylesheet" href="CSS/photoshop.css">
+
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+
+        <div id="photoshop"> <!--Photoshop start-->
+            <h1>Photoshop</h1> 
+            
+            <p class="tekst"> Photoshop er et billedbehandlingsprogram, hvor man kan redigere og manipulere billeder. Programmet er pixelbaseret modsat Illustrator som er vectorgrafik.</p> 
+            
+            <p class="tekst"> Programmet bruges primært til manipulation og behandling af billeder. 
+            I photoshop arbejder man i layers, det gør man bl.a. for at kunne arbejde non-destruktivt. Fordelen ved at arbejde i layers og non-destruktivt er, at man kan ændre sine handlinger og fortryde ændringer og manipulering man har skabt – Det kunne f.eks. være en fritlægning.</p> 
+            
+            <p class="tekst"> I photoshop kan du opsætte dit eget unikke interface, så det passer til dine behov, hvilket kan være tidsbesparende i sidste ende. Grundlæggende handler det om at sætte interfacet op, så det passer til din arbejdsgang – Men ellers er der nogle ganske udmærket defaults, som passer til de opgaver man gerne vil løse. </p> 
+            
+            <p class="tekst"> Det handler om at indrette sit workspace, så det er nemt og praktisk for dig. Det kan man gøre ved at placere de tools, som man bruger mest, der hvor det giver bedst mening. </p> 
+              
+        </div>  <!--Illustrator slut-->
+            
+        <div id="keyboard"> <!--Keyboard shortcuts start-->
+            <h2> Keyboards Shortcuts</h2> 
+            
+                <p class="tekst"> De kan bruges til at gøre arbejdet nemmere og hurtigere.</p>
+            
+             <div id="shortcutcontainer">
+            
+                <img id="shortcuts" class="allebilleder" src="Billeder/shortcuts.jpg">
+                
+                <blockquote>Forskellige Shortcuts i Photoshop ↑</blockquote>            
+            </div> 
+            
+            
+        </div>  <!--Keyboard shortcuts slut-->
+            
+        <div id="artboards"> <!--Artboards start-->
+            <h2> Artboards </h2> 
+            
+                <p class="tekst"> Et artboard er en form for lærred, som giver mulighed for at arbejde på flere forskellige ting samtidig.</p>
+                
+                <p class="tekst">Den dag vi tegnede spøgelser brugte vi artboards. Her skulle vi bruge farver til at beskrive forskellige stemninger.</p>
+            
+            
+            <div id="ghostcontainer"> 
+            
+                <img id="ghost" class="allebilleder" src="Billeder/ghost.jpg">
+                
+                <blockquote>Spøgelser i artboards ↑</blockquote>            
+            </div> 
+            
+            
+        </div>  <!--Artboards slut-->    
+            
+        <div id="selections"> <!--Selection start-->
+            <h2> Selections</h2> 
+            
+                <p class="tekst"> Der findes forskellige slags selection tools. Det bruges til at markere en del af billedet, som man gerne vil arbejde med. 
+                Et eksempel kunne være quick selection tool, som hjælper med at markere. Det gør den ved at gætte sig frem til, hvad der skal markeres. Quick selection tool er smart til at foretage en hurtig markering af et billede.</p>
+            
+            <div id="quickcontainer"> 
+            
+                <img id="quick" class="allebilleder" src="Billeder/quick.jpg">
+                
+                <blockquote>Sådan ser Quick Selection-Tool ud ↑</blockquote>            
+            </div> 
+            
+            
+        </div>  <!--Selection slut-->    
+        
+        <div id="pen"> <!--Pen Tool start-->
+            <h2> Pen Tool</h2> 
+            
+                <p class="tekst"> Pen tool bruger du til at definere nogle punkter, hvorefter du kan bøje de linjer som bliver genereret imellem de forskellige punkter. Eksempel Pen tool kan bruges til at fritlægge spøgelse.</p>
+            
+            <div id="pentoolcontainer"> 
+            
+                <img id="pentool" class="allebilleder" src="Billeder/pentool.jpg">
+                
+                <blockquote>Sådan ser Pen-Tool ud ↑</blockquote>            
+            </div> 
+            
+            
+        </div>  <!--Pen Tool slut-->               
+            
+            
+            <div id="felt20"> &nbsp;</div> <!--holder footer nede på siden-->     
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
