e6e04fbd50ab147324d6abd223ea3a07b781416c
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:08:38 +0100

    Create tekstpro.php

---
+++ tekstpro.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head> 
+        
+        <title> Tekstproduktion</title>
+        <?php include "overordnet.inc" ?>
+        <link rel="stylesheet" href="CSS/tekstpro.css">
+
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+
+        <div id="produktion"> <!--Tekstproduktion start-->
+            <h1> Tekstproduktion</h1> 
+            
+            <p class="tekst"> Når man arbejder med tekstproduktion har man fokus på de 5 nyhedskriterier: væsentlighed, sensation, konflikt, identifikation, aktualitet. </p> 
+            
+           <p class="tekst"> Formålet med at bruge de 5 nyhedskriterier er, at opnå højere kvalitet i dit indhold i forhold til den målgruppe man kommunikerer til, og derved opnå flere delinger af sine links på sociale medier, som i sidste ende skaber mere indgående trafik til din side.</p> 
+              
+            <p class="tekst">Vi læser tekster på mange forskellige måder, det afhænger af hvilken kontekst vi læser den i.</p> 
+            
+            <p class="tekst">
+            -	Nogle gange scanner vi en side, dette giver et visuelt overordnet billede af teksten. <br>
+            -	Andre gange skimmer vi teksten for at opnå en større helhedsforståelse, uden at gå helt i dybden med teksten. <br>
+            -	Ellers læser vi teksten i de store billeder og de små elementer. <br>
+            -	Når man bruger checking, anvender man både skimming, scanning og læsning for at skabe sig et oveblik.</p>  
+            
+            <p class="tekst">En artikel er opbygget på den måde, at man i overskriften typisk indleder med en påstand, som bliver understøttet med argumentation nede i brødteksten. 
+            </p>
+            
+            <p class="tekst">Grundlaget for at skabe et godt website er, at definere hvad siden formål er, og hvem man kommunikerer til. Man skal det gøre klart, hvad siden skal kunne, skal den være oplysende, sælgende, servicerende osv.  
+            </p>  
+            
+            <p class="tekst">Det er vigtigt at have gode webtekster på sin hjemmeside, da det er med til at fastholde læserens interesse, hvilket gør at de bliver længere tid på siden. Derudover er gode webtekster også med til at forbedre din SEO og derved sikre bedre placering i søgemaskiner. Det er vigtigt at tænke SEO ind i alle webtekster.  
+            </p>  
+            
+            <p class="tekst">En webtekst skal ikke bare være en god historie, den skal i sidste ende være med til at skabe konvertering og call to action. 
+            </p>  
+                    
+        </div>  <!--Tekstproduktion slut-->
+            
+        <div id="nyhedstrekanten"> <!--Nyhedstrekanten start-->
+            <h2> Nyhedstrekanten</h2> 
+            
+            <p class="tekst"> Nyhedstrekanten er en model som vi bruger til opbygning af en nyhedshistorie. Den består af 5 faser, en indledning, uddybning, baggrund, sekundære emner, detaljer. 
+            </p> 
+            
+            <p class="tekst"> 
+            -	Indledningen beskriver nyheden (hvem, hvad, hvor, hvornår.)<br>  
+            -	Uddybningen indeholder forklarende information, som understøtter indledningen.<br> 
+            -	Baggrunden går dybere ned i historien.<br> 
+            -	Sekundære emner, når man henviser til andre lignende emner.<br>  
+            -	Detaljer beskriver helt ned i detaljen, hvad der foregår. 
+            </p> 
+            
+            <p class="tekst"> Nyhedstrekanten kan også bringes i spil, når man skriver webtekster. Her er det vigtigt, at få hovedbudskabet frem i din overskrift, og derefter uddybe med bagrund og detaljer længere nede på siden.</p> 
+            
+            <p class="tekst"> Hovedsagligt skal dine tekster være nemme at skimme. De skal være skrevet i lag, skab gerne luft i teksten, og brug gerne whitespace og links.</p> 
+            
+        </div>  <!--Nyhedstrekanten slut-->
+            
+         <div id="nyhedskriterier"> <!--Nyhedskriterier start-->
+            <h2> Nyhedskriterier</h2> 
+            
+            <p class="tekst"> I løbet af semesteret arbejdede vi med nyhedskriterier.</p> 
+            
+            <div id="aviscontainer">
+            
+                <a href="Nyhedskriterier.pdf"><img id="avis" class="allebilleder" src="Billeder/avis.jpg"></a>
+                
+                <blockquote> Læs hele rapporten her - Klik på billedet ↑ </blockquote> 
+                        
+            </div>               
+        </div>  <!--Nyhedskriterier slut-->    
+            
+        <div id="webtekster"> <!--Webtekster start-->
+            <h2> Webtekster</h2> 
+            
+            <p class="tekst"> Her skulle vi skrive webtekst til en given virksomhed</p> 
+            
+            <div id="webtekstcontainer">
+            
+                <a href="webtekster.pdf"><img id="webtekst" class="allebilleder" src="Billeder/webtekst.jpg"></a>
+                
+                <blockquote> Læs hele rapporten her - Klik på billedet ↑ </blockquote> 
+                        
+            </div>               
+        </div>  <!--Webtekster slut-->      
+        
+            
+            <div id="felt15"> &nbsp;</div> <!--holder footer nede på siden-->     
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
