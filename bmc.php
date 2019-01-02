f885e1150ca3ca8f4cc84b8124bf77957ba1c03c
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:05:57 +0100

    Create bmc.php

---
+++ bmc.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head> 
+        
+        <title> Business Model Canvas</title>
+        <?php include "overordnet.inc" ?>
+        <link rel="stylesheet" href="CSS/bmc.css">
+
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+
+        <div id="forretning"> <!--Forretningsforståelse start-->
+            <h1> Forretningsforståelse</h1> 
+            
+            <p class="tekst"> Forretningsforståelse handler om at forstå en forretning på forskellige niveauer, altså en form for helhedsforståelse. </p> 
+            
+            <p class="tekst"> Man bør starte med at præcisere, hvilken type virksomhed det drejer sig om. Er det en produktionsvirksomhed, handelsvirksomhed, servicevirksomhed eller noget helt andet.</p> 
+              
+            <p class="tekst">Efter virksomhedstypen er defineret, er der en række andre spørgsmål man kan stille f.eks. hvem er virksomheden, hvor er virksomheden, hvem er virksomheden skabt for, hvad kan virksomheden gøre osv! Det handler om at stille spørgsmål, som skal få dig til at reflektere over virksomheden og dens mission.  
+            </p>  
+            
+        <div id="understandingcontainer">
+            
+                <img id="understanding" class="allebilleder" src="Billeder/understanding.jpg">
+                                        
+        </div>       
+            
+        </div>  <!--Forretningsforståelse slut-->
+            
+            
+        <div id="value"> <!--Værdikæde start-->
+            <h2> Værdikæden </h2> 
+            
+            <p class="tekst"> Værdikæden er en teori, som vi bruger til at følge en proces hele vejen igennem en virksomhed. F.eks. i en produktionsvirksomhed:</p> 
+            
+            <p class="tekst">       
+            1.	Til at starte med har vi Input – Det er her at vi modtager råvarer og materialer.<br>
+            2.	Dernæst kommer vi over i produktionen – Det er her, at vi begynder at producere noget af vores råvarer.<br>
+            3.	Derefter kommer vi til markedsføring og service - Det er her at produktet bliver markedsført og promoveret.<br>  
+            4.	Til sidst har vi Output: Det er solgte varer og fortjeneste. 
+            </p> 
+            
+            <div id="businesscontainer">
+            
+                <img id="business" class="allebilleder" src="Billeder/business.jpg">
+                
+                <blockquote> Value Chain ↑</blockquote> 
+                        
+            </div> 
+            
+        </div>  <!--Værdikæde slut-->
+            
+        <div id="canvas"> <!--canvas start-->
+            <h2>Business Model Canvas</h2> 
+            
+            <p class="tekst"> Vi har arbejdet med Business Model Canvas i klassen. Her fik vi til opgave at analysere Ryan Air ud fra Business Model Canvans modellen.</p>
+            
+            <p class="tekst"> Her er skulle vi igennem de forskellige segmenter i BCM.</p> 
+                
+            <p class="tekst">BMC hjælper dig med at skabe værdi for din forretning. (Daugaard, 2018, S.15, Forretningsforståelse MMD 07112018)</p>  
+            
+            <p class="tekst"> 
+            Hvor value proposition hjælper dig med at skabe værdi for dine kunder.</p> 
+            
+        <div id="canvascontainer">
+            
+                <img id="canvasbillede" class="allebilleder" src="Billeder/canvas.jpg">
+                
+                <blockquote> Business Model Canvas ↑</blockquote>            
+        </div> 
+            
+        </div>  <!--canvas slut-->   
+            
+        <div id="arketyper"> <!--De 7 arketyper start-->
+            <h2>De 7 arketyper</h2> 
+            
+            <p class="tekst"> Her skal skrives noget om de 7 arketyper</p> 
+            
+        </div>  <!--De 7 arketyper slut-->     
+            
+        
+            <div id="felt17"> &nbsp;</div> <!--holder footer nede på siden-->     
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
