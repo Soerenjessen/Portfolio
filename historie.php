67cf972bc95ad92b53e793d5f5210e7547483923
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:07:44 +0100

    Create historie.php

---
+++ historie.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head> 
+        
+        <title> Historie</title>
+        <?php include "overordnet.inc" ?>
+        <link rel="stylesheet" href="CSS/historie.css">
+
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+            
+         <div id="periode"> <!--Webbets Perioder-->
+            
+            <h1>Webbets Perioder</h1>
+            
+            <p class="tekst"> Der er forskellige perioder af internettets historie. Typisk inddeles de i disse fem perioder:</p> 
+            
+            <p class="tekst"> 1. Tidlige eksperimenter (1993-1995).</p> 
+        
+            <p class="tekst">2.	Under konstruktion (fra 1996).</p> 
+             
+            <p class="tekst">3.	Semantisk søgning (fra 2000).</p> 
+             
+            <p class="tekst">4.	Sociale netværk (2003).</p> 
+             
+            <p class="tekst">5.	Det mobile web (2010).</p>  
+             
+            <p class="tekst"> Jeg vil herunder kort beskrive de forskellige perioder, og sætte fokus de vigtigste historiske nedslag.</p> 
+                      
+        </div>  <!--Webbets Perioder slut-->    
+            
+            
+        <div id="eksperimenter"> <!--Tidlige Eksperimenter-->
+            
+            <h2>Tidlige Eksperimenter (1993-1995)</h2>
+            
+            <p class="tekst"> I 1993 begynder udviklingen af den første brugbare browser NCSA Mosaic, som er med til at sætte gang i videreudviklingen af webbet.</p> 
+            
+            <p class="tekst"> Det der kendetegner denne periode er, at forskellige individer, virksomheder, organisation eksperimenter med, hvordan webbet skal se ud. Der er ikke nogen officiel syntaks, eller guideline til hvordan webbet skal se ud. Denne periode er en form for det vilde vesten.</p> 
+        
+            <p class="tekst">I denne periode var HTML-sproget og browserne teknisk begrænsende i forhold til design og layout. Websiderne var opbygget af simpel hypertekst, uden fokus på design. Stilarten på mange af siderne var det klassiske bogformat. Dette var ikke ligefrem med til at øge brugervenligheden på siderne.</p> 
+             
+            <p class="tekst">Det er generelt en periode, hvor der er meget uvished om hvilken retning webbet skal udvikle sig. I994 udkommer Netscape 1.1, hvor det var muligt at arbejde med HTML tabeller, og det blev muligt at indsætte baggrundsbilleder ved hjælp af GIF og JPEG, hvilket gav helt nye designmuligheder.</p> 
+                      
+        </div>  <!--Tidlige Eksperimenter slut-->    
+            
+            
+            
+        <div id="konstruktion"> <!--Under Konstruktion-->
+            
+            <h2>Under Konstruktion (1996-2000)</h2>
+            
+            <p class="tekst">I denne periode opstår webguruernes kamp. </p> 
+            
+            <p class="tekst"> På den ene side var David Siegel, som var meget visuelt orienteret. Han fokuserede meget på layout og design, og havde en meget kreativ tankegang om, hvordan websites kunne tage sig ud. David Siegel havde meget fokus på design og avancerede funktioner, som skulle gøre webbet et mere interessant sted at befinde sig. </p> 
+        
+            <p class="tekst">På den anden side var danske Jacob Nielsen, som havde en anden filosofi omkring webbet. Jacob Nielsens fokus var på brugervenlighed og funktion. I løbet af 1990erne opnår han status som webguru, og udgiver forskellige videnskabelige bøger omkring usability. I forhold til design, havde han en mere minimalistisk tilgang end David Siegel. I dag vil man nok mene, at Jacob Nielsen havde en meget firkantet tankegang omkring webudviklingen, da han satte brugervenlig før design. </p> 
+             
+            <p class="tekst">Hvorimod David Siegel forsøgte at forene design, teknik og indhold på websider. Til trods må man den dag i dag, stadig anerkende mange af Jacob Nielsens tanker og anskuelser. </p> 
+                      
+            <p class="tekst">I samme periode kører browser-krigen, som er en kamp imellem forskellige browserudviklere. I denne periode konkurrerede Netscape med Microsofts, Internet Explorer, om at vinde webmarkedet. Her havde Microsoft den fordel, at de installerede Internet Explorer på deres egne computere, hvilket sikrede dem stort set hele markedet.  </p> 
+            
+            <p class="tekst">Derudover var Internet Explorer den første browser, som understøttede fremtidens styllingsprog Cascading Style Sheet, bedre kendt som CSS.</p> 
+            
+        </div>  <!--Under Konstruktion slut-->    
+            
+            
+            
+        <div id="semantik"> <!--Semantisk Søgning-->
+            
+            <h2>Semantisk Søgning (Fra 2000)</h2>
+            
+            <p class="tekst">En af vigtige hændelser i denne periode er stiftelsen af søgemaskinen Google. Google hjalp brugerne med at finde det indhold, som de søgte på nettet. Google benyttede sig af en algoritme, som rankede siderne efter semantisk betydning. Det medførte til en helt ny industri indenfor søgemaskine optimering, bedre kendt som SEO.</p> 
+            
+            <p class="tekst"> Googles tanke om at webbet skulle være et sted, hvor alle kunne finde og bidrage med nyt indhold, passede i fin tråd med Tim Berners Lees grundide omkring webbet.</p> 
+            
+        </div>  <!--Semantisk Søgning slut-->     
+            
+            
+            
+                 
+        <div id="network"> <!--Sociale Netværk-->
+            
+            <h2>Sociale Netværk (2003-2010)</h2>
+            
+            <p class="tekst">I denne periode opstår nye forskellige sociale medier, som f.eks. MySpace og Facebook.</p> 
+            
+            <p class="tekst"> En af de første sociale medier er mediet MySpace, som har fokus på musik. MySpace bliver populært, da musikere og kunstnere nu selv kan udgive deres egen musik uden om pladeselskaber, hvilket revolutionerer hele musikbranchen på meget få år. MySpace bliver et forum, hvor folk kan finde nichemusik, som ikke bliver spillet i mainstream radio og tv. Dette tiltrækker store grupper af mennesker, som interesser sig for f.eks. Metal, Rap, Hiphop og mange andre genrer, som på dette tidspunkt var nichegenre. </p> 
+            
+            <p class="tekst"> I forbindelse med at Facebook opnåede op for brugere i september 2005, oplevede MySpace en voldsom nedgang i brugere. Forskellen på MySpace og Facebook, var at på Facebook, kunne man følge sine venner, spille spil, læse nyheder osv. Muligheden for at kommunikere på Facebook var langt bredere end på MySpace. På Facebook kunne man deltage i debatter, skrive opslag på sin væg, skrive privatbeskeder osv.</p> 
+            
+        </div>  <!--Sociale Netværk slut--> 
+            
+            
+        
+        <div id="mobileweb"> <!--Det mobile web-->
+            
+            <h2>Det Mobile Web (Fra 2010)</h2>
+            
+            <p class="tekst">I 2007 lancerede Apple den første smartphone, som kunne tilgå internettet. Det revolutionerende ved IPhone og senere Tablet (2010) var hele ideen med touchscreen. Man behøvede ikke længere et tastetur, og på den måde blev telefonen en ny form for computer.</p> 
+            
+            <p class="tekst"> I løbet af få år blev de mobile webbrugere i overtal, og i 2010 var der flere brugere på det mobile web. Brugerne nægtede at anvende hjemmesider, som ikke var opdateret til telefonformatet. Det var i den forbindelse at responsive design, så dagens lys.  </p> 
+            
+            <p class="tekst"> Herefter ændrede webudvikling sig i retning af det vi kender i dag, hvor vi designer hjemmesider til mobilen først, og herefter til andre enheder som Tablet og Desktop.</p> 
+            
+        </div>  <!--Det mobile web--> 
+            
+    
+
+        <div id="historien"> <!--Historie om internettet-->
+            <h2> Hvad er forskellen på begreberne "Internettet" og "World Wide Web" </h2> 
+            
+            <p class="tekst"> Internettet er lig med infrasturktur = World Wide Web er en applikation på den infrastruktur. 
+            (eksempel Metafor = Vejnettet er internettet: Busser, biler er applikationer på vejnettet).
+            </p> 
+            
+            <h2>Hvad kendetegner udviklingen af internettet?</h2>
+            
+            <p class="tekst"> Protokoller har været afgørende for internettet. Det er et regelsæt (en aftale), som gør at vi kommunikerer på det samme sprog. </p> 
+                
+            <h2>Hvorfra kommer begrebet "Inter-net" og hvad hentyder det til?</h2>
+            
+            <p class="tekst">Begrebet stammer fra den Franske afdeling Cyclades – På grund af manglende budget, ville man have maskiner til at kommunikere sammen = Deraf navnet Inter-net.</p> 
+            
+            <h2>Hvornår startede udviklingen af internettet?</h2>
+            
+            <p class="tekst">Udviklingen af internettet startede tilbage i 1957. Det startede under den kolde krig, da Sovjetunionen, som den første nation i verden, sendte den ubemandede rumsonde Sputnik i kredsløb rundt om jorden. I 1966 begyndte man arbejde på ARPANET, som var et samarbejde imellem lande som England, USA og Frankring. De forskellige interesser heri var militære, videnskabelige og kommercielle</p>
+            
+            <h2> Hvad er "information overload"</h2>
+            
+            <p class="tekst">Information overload opstod omkring starten af år 1900. Mængden af viden der skulle håndteres, var ganske enkelt blevet uoverskuelig. Man ønskede at organisere viden, og gøre den tilgængelig for alle.</p>
+            
+            <h2> Hvad var ideen bag "World Palace of Knowledge"</h2>
+            
+            <p class="tekst">Folkene bag World Palace of Knowledge var Paul Otlet og Henri LaFontaine som havde en vision om, at verdensfred kunne opnås ved global vidensdeling. De begyndte at samle og organisere viden, i verdens første manuelle søgemaskine. 
+
+            De skabte i bund og grund et kæmpe arkiv med over 50 millioner dokumenter og videofilm. Når almindelige mennesker sendte spørgsmål til World Palace of Knowledge, var der ansatte, som fandt alt information der var på området, men det helt unikke var, at de også fandt relateret artikler til det specifikke emne, altså en form for linking. 
+            </p>
+            
+            <h2>Hvad var ideen bag "The Memex"?</h2>
+            
+            <p class="tekst">Vannevar Bush.
+            Det samme princip som ”World Palace of Knowledge” men mere automatisk. Brug af keywords, for at finde de rigtige informationer, det gjorde man ved at benytte association (Linking). 
+            </p>    
+            
+            <h2>Hvem var Douglas Engelbart og hvad er han kendt for?</h2>
+            
+            <p class="tekst">Pioneer indenfor udviklingen af computer tilbage i slutningen af 60erne. Hans forskerteam opfandt bl.a. ”The Mouse” musen. De forsøgte at lave værktøjerne til i praksis at kunne dele information.</p> 
+            
+            <h2>Hvem opfandt begrebet "Hypertext" og hvad kunne det første Hypertext System.</h2>
+            
+            <p class="tekst">-	Ted Nelson – Man kunne skrive ord og åbne flere vinduer. Han anså som alt tekst forbundet. Deraf navnet Hypertext. </p> 
+            
+            <h2>Hvordan inspirerede Tim's far's artikel Tim?</h2>
+            
+            <p class="tekst">Den inspirerede ham til, hvordan en computer kan etablere links ligesom den menneskelige hjerne gør. Hjernen linker ting som har en fælles funktion, selvom de i første omgang kan virke fuldstændig forskellige. F.eks. en hest - Det kan være en hest på en mark, det kan være en hest på et skakbræt, det kan være en hest i gymnastiksal. 
+            Det var denne sammenhæng mellem tekst, som man kalder hypertekst der inspirerede Tim Berners Lee. Han arbejdede med ideen om at lære computeren at læse hypertekst. 
+            </p> 
+            
+            <h2>Hvad var programmet Enquire</h2>
+            
+            <p class="tekst">Da Tim Berners Lee kom til Cern i 1980, ville han gerne have et overblik over, hvem der stod for hvad, og hvilke computerprogrammer de brugte. For at få et overblik skabte han Enquire, som var et program der kunne sammenligne dokumenter omkring ansatte, deres projekter, og den software de brugte. Enquire kunne tjekke om dokumenter indeholdte nogle specifikke keywords. Hvis det gjorde, var deres lokation i dokumentet inter linked.</p> 
+            
+            <h2>Hvad var problemet med ARPANET?</h2>
+            
+            <p class="tekst">Problemet med Arpanet var, at det blev brugt af forskellige typer computere. Dem som arbejdede med en slags computer, havde brug for en grundlæggende viden om andre computere for at kunne genkende data herfra, da computere havde forskellige styresystemer, kunne de ikke forstå hinanden. </p> 
+            
+            <h2>Hvad var Tim's ide?</h2>
+            
+            <p class="tekst">Tim Berners Lees ide var at udvikle et fælles computersprog, så alle brugere på netværket kunne læse hinandens data, uafhængigt af hvilket computersystem de brugte. </p> 
+            
+            <h2>Hvad står HTML og URL for?</h2>
+            
+            <p class="tekst">HTML står for Hyper Text Markup Language og URL står for Universal Ressource Locator.</p> 
+            
+            <h2> Hvad er en Browser?</h2>
+            
+            <p class="tekst">En browser er et program, som viser et billede af HTML dokumenter på en computer. </p> 
+            
+            <h2>Hvem var Marc Andreasen?</h2>
+            
+            <p class="tekst">Marc Andreasen var en ung studerende som skabte browseren Mosaic. Mosaic indeholdte både hyperlinks og billeder. </p> 
+            
+        </div>  <!--Historie om Internettet slut-->
+            
+            <div id="felt3"> &nbsp;</div> <!--holder footer nede på siden-->     
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
