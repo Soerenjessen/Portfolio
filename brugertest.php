64329270c3d9df884c3dc89ebb4321e6abd386ea
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:05:59 +0100

    Create brugertest.php

---
+++ brugertest.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head> 
+        
+        <title> Brugertest</title>
+        <?php include "overordnet.inc" ?>
+        <link rel="stylesheet" href="CSS/brugertest.css">
+
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+
+        <div id="videnskabsteori"> <!--Videnskabsteori start-->
+            <h1> Videnskabsteori</h1> 
+            
+            <p class="tekst"> Videnskabsteori handler om hvad videnskab egentlig er, og hvordan vi forholder os til viden, og producerer viden. Vi bruger den videnskabelige metode, som et værktøj til at undersøge ting.  
+            Vi bruger den til at tilsidesætte vores egne antagelser, og fokuserer på hvad der er sandhed i forhold til et givent problem. </p> 
+            
+            <p class="tekst"> Der kan være mange forskellige antagelser om sandhed. Er der en sand virkelighed, eller er alle sandheder relative. Det kommer an på, hvordan vi perciperer virkeligheden. 
+            Som Søren Kirkegaard skrev ”Livet er ikke et problem der skal løses, men en virkelighed der skal opleves” – Vi kan altså opleve virkeligheden på mange forskellige måder, den påvirkes af vores kultur, opdrag og generelle eksistentielle værdier. 
+            Det er vigtigt, at vi forstår hvordan mennesker kan opfatte verdenen og sandheder forskelligt, og den ene sandhed er ikke nødvendigvis mere rigtig end den anden.</p> 
+              
+            <p class="tekst">Det eksempel vi beskæftigede os med på klassen, var den blå/sorte kjole. Teknisk set er kjolen Sort/Blå, men en del i klassen opfattede kjolen som Guld/Sort. Hvad er så rigtigt og forkert? Rent videnskabeligt er kjolen blå og sort, men vi kan ikke benægte at andre opfatter den som guld og hvid. 
+            </p> 
+            
+            <div id="kjolecontainer">
+            
+                <img id="kjole" class="allebilleder" src="Billeder/kjole.jpg">
+                
+                <blockquote> Den omdiskuterede kjole ↑</blockquote> 
+                        
+            </div>   
+            
+        </div>  <!--Videnskabsteori slut-->
+            
+            
+        <div id="positivisme"> <!--Positivisme start-->
+            <h2> Positivisme </h2> 
+            
+            <p class="tekst"> Positivisme er en videnskabsmetode som udspringer fra naturvidenskaben. Man arbejder ud fra, at der kun er en virkelighed. Her har man fokus på ting, som kan måles og vejes. 
+            Derfor er begreber som følelser, religion, etik, moral, kunst, kultur osv. ikke positivisme, da vi ikke kan måle det.(https://www.religion.dk/leksikon/positivisme)</p> 
+            
+            <p class="tekst"> Et eksempel på positivisme er ”Jorden er rund” – Vi kan måle og veje den.</p> 
+            
+          <div id="weightcontainer">
+            
+                <img id="weight" class="allebilleder" src="Billeder/weight.jpg">
+                        
+          </div> 
+              
+            
+        </div>  <!--Positivisme slut-->
+            
+        <div id="hermeneutik"> <!--Hermeneutik start-->
+            <h2>Hermeneutik</h2> 
+            
+            <p class="tekst"> Hermeneutikken udspringer fra den humanistiske videnskab. Med den hermeneutiske videnskab kan vi analysere vores fortolkninger.(https://videnskab.dk/kultur-samfund/hvad-er-hermeneutik). 
+            </p>
+            
+            <p class="tekst"> Det handler om vores subjektive tolkning af vores omverden. 
+            Der behøver nødvendigvis ikke at være nogle endegyldige sandheder. Flere fortolkninger understøtter hinanden frem for at udelukke hinanden.(Daugaard, 2018, S.14, MMD brugertest 10102018). 
+            </p> 
+                
+            <p class="tekst">I den hermeneutiske spiral arbejder vi med forståelse, dernæst fortolkning, og tilbage til forståelse/forforståelse, sådan veksler man imellem disse to faser. </p>  
+            
+        </div>  <!--Hermeneutik slut-->
+            
+        <div id="overvejelser"> <!--Metodiske overvejelser start-->
+            <h2> Metodiske Overvejelser </h2> 
+            
+            <p class="tekst"> Metodiske overvejelser handler om, hvordan udvikler man et undersøgelsesdesign, som bedst muligt besvarer mine spørgsmål (Daugaard, 2018, S.18, MMD brugertest 10102018) 
+            Det kan man gøre ved at starte med at definere, hvad vil man undersøge? Hvilke spørgsmål kan være relevante at få svar på, og hvad skal undersøgelsens svar bruges til. Det er vigtigt at indsamle valid og brugbar viden, for at sikre en god undersøgelse
+            </p> 
+            
+            <p class="tekst"> Til dette kan bruges sekundære og primære datakilder. Her beskæftiger vi os med de forskellige researchparadigmer.  
+            kvalitative og kvantitative metode.</p>
+            
+        </div>  <!--Metodiske overvejelser slut-->    
+            
+        <div id="kvalitativ"> <!--Kvalitativ metode start-->
+            <h2>Kvalitativ metode</h2> 
+            
+            <p class="tekst"> I den kvalitative metode arbejder man med data, som ikke kan beskrives i konkrete talstørrelser f.eks. holdninger, adfærd osv. Det er indsamling af bløde data.  Den kvalitative metode handler om at forstå de følelser og overvejelser respondenterne gør sig.(Daugaard, 2018, S.22, MMD brugertest 10102018). 
+            Der findes nogle forskellige metoder hertil f.eks. interviews og observationer. I forbindelse med interviews vil man stille mange ”hv-spørgsmål” for at opnå en større forståelse. Ofte vil besvarelsen man får tilbage, være i form af lange interviews og samtaler med en eller flere personer.</p> 
+            
+            <p class="tekst"> Selve databearbejdningen består primært af at fortolke og analysere de svar man får tilbage fra interviews og samtaler. Fordelen ved denne metode er, at det giver et meget dybere og nuanceret billede af en problemstilling.
+            </p> 
+            
+        </div>  <!--Kvalitativ metode slut-->     
+            
+        <div id="kvantitativ"> <!--Kvantitativ start-->
+            <h2> Kvantitativ </h2> 
+            
+            <p class="tekst"> I den kvantitative metode arbejder man med konkret og målbart data. Det vil sige data, hvor det er muligt at sætte tal på. Det kunne f.eks. være spørgeskemaer, meningsmålinger osv. Her kan vi statistisk måle og veje vores data.  
+            Når vi anvender den kvantitative metode, handler det ikke om at forstå, hvorfor folk mener, som de mener. Vi er dybest set ligeglade med om vi forstår deres overvejelser, det handler om at vi kan forklare dem.</p> 
+            
+            <p class="tekst"> Den kvantitative metode anvendes typisk ved større undersøgelser, hvor den kvalitative metode typisk anvendes ved mindre, men dybere undersøgelser. 
+            </p> 
+              
+            <p class="tekst">Fordelen ved den kvantitative metode er, at vi kan generalisere ud fra vores resultater. Den data man behandler, vil typisk være besvarelser af spørgeskemaer, eller notater fra observationer – Igen konkrete og målbare resultater.(Daugaard, 2018, S.24, MMD brugertest 10102018).</p> 
+                    
+        </div>  <!--Kvantitativ slut-->
+            
+        <div id="btest"> <!--Brugertest start-->
+            <h2> Brugertest </h2> 
+            
+            <p class="tekst"> Hele ideen med at foretage brugertest er, at inddrage brugerne tidligt i processen. Som multimediedesigner handler det om at inddrage brugerne, som en aktiv medudvikler på digitale løsninger. I sidste ende er brugerinddragelse er med til at højne kvaliteten på slutløsningen, da brugerne er med til at validere løsningerne i forhold til kommunikation, navigation, brugervenlighed osv.</p> 
+            
+            <p class="tekst"> Det er vigtigt at huske, at en test ikke skal bevise noget, den skal blot informere. Når man foretager brugertest omkring brugervenlighed (usability), er det ikke et engangsshow. Det er en iterativ proces, dvs. at det er noget man er nødt til at undersøge løbende.</p> 
+                          
+        </div>  <!--Brugertest slut-->  
+            
+        <div id="gangstertest"> <!--gangstertest start-->
+            <h2> Kortsortering </h2> 
+            
+            <p class="tekst"> Vi foretog en kortsortering i forbindelse med vores projekt omkring Bettestrup is</p> 
+            
+        <div id="kortsort2container">
+            
+                <img id="kortsort2" class="allebilleder" src="Billeder/kort2.jpg">
+                
+                <blockquote> Kortsortering fra Bettestrup is ↑</blockquote> 
+                        
+        </div>      
+                                  
+        </div>  <!--gangstertest slut-->     
+        
+            <div id="felt16"> &nbsp;</div> <!--holder footer nede på siden-->     
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
