daf6aa2d61d3dffbbf801c0ef33f3c4cba4034d5
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-02 20:08:36 +0100

    Create present.php

---
+++ present.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head> 
+        
+        <title> Præsentationsteknik</title>
+        <?php include "overordnet.inc" ?>
+        <link rel="stylesheet" href="CSS/present.css">
+
+
+     </head>
+    <body>
+        
+    <?php include "menu.inc" ?>    
+        
+        <div id="pagewrap">
+
+        <div id="intro"> <!--Introduktion start-->
+            <h1> Præsentationsteknik</h1> 
+            
+            <p class="tekst"> Når man arbejder med præsentationsteknik er der fem overordnet hovedformål, som er med til at sikre en solid præsentation.</p> 
+            
+           <p class="tekst"> 
+               1.	Informere<br> 
+               2.	Overtale<br> 
+               3.	Uddanne<br> 
+               4.	Underholde<br> 
+               5.	Skabe og fastholde interesse.(Daugaard, 2018, S.9-10, Kommunikation Præsentationsteknik - MMD 18092018).  
+            </p> 
+              
+            <p class="tekst">Du skal have styr på kommunikationssituationen. Hvem er dine modtagere, dit budskab, og dig selv som afsender. Tænk over, hvem siger hvad til hvem. 
+            </p> 
+            
+        <div id="fiskecontainer">
+            
+                <img id="fisk" class="allebilleder" src="Billeder/fisk.jpg">
+                        
+        </div>     
+            
+                    
+        </div>  <!--Introduktion slut-->
+            
+            
+        <div id="opbygning"> <!--Talens Opbygning start-->
+            <h2> Talens opbygning</h2> 
+            
+            <p class="tekst"> Er du en kæmpe klaptorsk til at holde taler, så kan du med stor gavn anvende fiskemodellen. 
+            </p> 
+                      
+            
+        </div>  <!--Talens opbygning slut-->
+                
+            <div id="felt14"> &nbsp;</div> <!--holder footer nede på siden-->     
+        </div> <!--pagewrap slut-->
+        
+            <?php include "footer.inc" ?>    
+
+    </body>
+</html>
