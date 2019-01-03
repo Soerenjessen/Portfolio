9ecb08d0c5adc668ae4fb8c03bbb2091bb8c41bb
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-04 00:56:07 +0100

    Update present.php

--- present.php
+++ present.php
@@ -30,24 +30,65 @@
             <p class="tekst">Du skal have styr på kommunikationssituationen. Hvem er dine modtagere, dit budskab, og dig selv som afsender. Tænk over, hvem siger hvad til hvem. 
             </p> 
             
-        <div id="fiskecontainer">
+        <div id="pteknikcontainer">
             
-                <img id="fisk" class="allebilleder" src="Billeder/fisk.jpg">
-                        
+                <img id="pteknik" class="allebilleder" src="Billeder/pteknik.jpg">   
         </div>     
             
                     
         </div>  <!--Introduktion slut-->
             
             
-        <div id="opbygning"> <!--Talens Opbygning start-->
-            <h2> Talens opbygning</h2> 
+        <div id="kropsprog"> <!--Kropsprog start-->
+            <h2> Kropsprog</h2> 
             
-            <p class="tekst"> Er du en kæmpe klaptorsk til at holde taler, så kan du med stor gavn anvende fiskemodellen. 
+            <p class="tekst"> Når man skal holde taler, fremlægge, præsentere osv. Er det vigtigt, at man er bevidst omkring sin non-verbale kommunikation. Kropsproget spiller en stor rolle, når man præsenterer. I den forbindelse har vi arbejdet med nogle forskellige teknikker, som skal hjælpe med at holde styr på den del. 
+            </p> 
+            
+            <p class="tekst"> Det nonverbale kropsprog siger ufatteligt meget om din gennemslagskraft og troværdighed. Det er derfor vigtigt, at der er overensstemmelse mellem det du siger, og det dit kropssprog signalerer, ellers kan din kommunikation nemt blive fejlfortolket. 
+            </p> 
+            
+            <p class="tekst"> Man skal derfor have fokus på kropsholdning, stemmeleje, taletempo, mimik, ansigtsudtryk, øjenbevægelser, gestik, påklædning, afstand til tilhørere osv.
+            </p> 
+            
+            <p class="tekst"> Derfor kan det være en god ide at klæde sig rigtigt på til lejligheden. Hvis man nu f.eks. skal holde et oplæg for smedelærlinge, så vil det være distancerende, hvis man mødte op i jakkesæt og slips. Omvendt, hvis du skulle til jobsamtale på Esplanaden, så vil det virke forkert, hvis du dukkede op i blå kedeldragt.  
+            </p> 
+            
+            <p class="tekst"> De tilhørende kan danne sig mange forskellige meninger ud fra den kommunikation dit kropssprog og udtryk sender.  
             </p> 
                       
             
-        </div>  <!--Talens opbygning slut-->
+        </div>  <!--Kropsprog slut-->
+            
+            
+        <div id="forberedelse"> <!--forberedelse start-->
+            <h2> Præsentationsforberedelse</h2> 
+            
+            <p class="tekst"> Når man forbereder en præsentation, bør man stille sig selv spørgsmålene, hvad skal tilhørerne have ud af denne præsentation, hvad er det væsentlige budskab, og hvordan vil jeg formidle det.
+            </p> 
+            
+            <p class="tekst"> I løbet af semestret læste vi et kompendie skrevet af Mette Hald, som handler om, hvordan du kan tilrettelægge din præsentation ud fra fem forarbejdningsfaser. 
+            </p> 
+            
+            <p class="tekst"> Først skal man finde stoffet og sætte sig ind i det. Dernæst bør man disponere stoffet, så man holder fokus på det væsentlige. Derefter skal man omformulere stoffet, så det bliver sagt med dine egne ord. Fjerde step er at øve sig på sin præsentation. Femte step, er selve præsentationen.    
+            </p> 
+            
+            <p class="tekst"> Til at disponere kan man bruge fiskemodellen. Den har vi bl.a. arbejdet med her på 1. semester.</p> 
+            
+            <p class="tekst"> Først skal man have styr på indledningen. Det er en meget vigtig fase, hvor førstegangsindtrykket til lytterne gives, og det her at man skal fange deres opmærksomhed. </p> 
+                      
+            
+            <p class="tekst"> Anden del er selve hoveddelen, som udgør ca. 80% af indholdet. Det er i denne fase, at lytterne skal præsenteres for dine hovedargumenter.</p> 
+                      
+            
+            <p class="tekst"> Til sidst er der afslutningen, hvor du runder af med at opsummere dine pointer, og får lytterne til at huske på dit budskab.</p> 
+                      
+                      
+            
+        </div>  <!--forberedelse slut-->    
+            
+            
+            
                 
             <div id="felt14"> &nbsp;</div> <!--holder footer nede på siden-->     
         </div> <!--pagewrap slut-->
