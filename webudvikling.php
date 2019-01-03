a7fa6e44a7433f5405115105033f3078499bb792
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-04 00:56:20 +0100

    Update webudvikling.php

--- webudvikling.php
+++ webudvikling.php
@@ -16,7 +16,7 @@
         <a href="tml.php"><div id="tml"> <!--HTML box start-->
             <div id="tmltekst">
                 <h1> HTML</h1>
-                <p> Semantiske tags, Struktur, Elementer, Kodeskik, Editor osv</p>
+                <p> Semantiske tags, Struktur, Elementer, Kodeskik, Editor osv.</p>
             </div>
                 <div id="tmlbillede">    
                     <img id="tmlbillede" class="allebilleder" src="Billeder/htmlbillede400px.jpg"> 
@@ -26,7 +26,7 @@
         <a href="historie.php"><div id="historie"> <!--internettets historie box start-->
             <div id="historietekst">
                 <h1> Internettets Historie</h1>
-                <p> Historien bag World Wide Web og dens pionerer </p>
+                <p> Historien bag World Wide Web og dens pionerer. </p>
             </div>
                 <div id="historiebillede">    
                     <img id="historiebillede" class="allebilleder" src="Billeder/historiebillede400px.jpg"> 
@@ -36,7 +36,7 @@
         <a href="webserver.php"><div id="webserver"> <!--webserver box start-->
             <div id="servertekst">
                 <h2> Webserver</h2>
-                <p> Her kan du finde information om Server, Klienter, FTP Server osv </p>
+                <p> Her kan du finde information om Server, Klienter, FTP Server osv. </p>
             </div>
                 <div id="serverbillede">
                     <img id="serverbillede" class="allebilleder" src="Billeder/serverbillede400px.jpg">
@@ -46,7 +46,7 @@
         <a href="css.php"><div id="css"> <!--CSS box start-->
             <div id="csstekst">
                 <h2> CSS</h2>
-                <p> Kaskaderegler, Styling, Layout, Class, Id, Selectors osv</p>
+                <p> Kaskaderegler, Styling, Layout, Class, Id, Selectors osv.</p>
                 
             </div>
                 <div id="cssbillede">
@@ -57,7 +57,7 @@
         <a href="plan.php"><div id="planning"> <!--Planlægning box start-->
             <div id="plantekst">
                 <h2> Planlægning og Overordnet Design</h2>
-                <p> Det Bette Ismejeri</p>
+                <p> Det Bette Ismejeri.</p>
                 
             </div>
                 <div id="planbillede">
@@ -68,7 +68,7 @@
         <a href="work.php"><div id="workshop"> <!--Workshop box start-->
             <div id="worktekst">   
                 <h2> HTML og CSS Workshop</h2>
-                <p> HTML Tabeller og Styling af dem</p>
+                <p> HTML Tabeller og Styling af dem.</p>
                 
             </div>
                 <div id="workbillede">
@@ -79,7 +79,7 @@
         <a href="grid.php"><div id="grids"> <!--CSS Grid box start-->
             <div id="gridtekst">
                 <h2> CSS Grid</h2>
-                <p> Responsive Webdesign, Grid Container, Mobile First</p>
+                <p> Responsive Webdesign, Grid Container, Mobile First.</p>
             
             </div>
                 <div id="gridbillede">
@@ -90,7 +90,7 @@
         <a href="version.php"><div id="version"> <!--Versionsstyring box start-->
             <div id="versiontekst">
                 <h2> Versionsstyring</h2>
-                <p> Github, Protokoller, projektmapper, Commits osv</p>
+                <p> Github, Protokoller, Projektmapper, Commits osv.</p>
             
             </div>
                 <div id="versionbillede">
@@ -101,7 +101,7 @@
         <a href="arktiktur.php"><div id="ia"> <!--Informationsarkitektur box start-->
             <div id="iatekst">
                 <h2> Informations-<br>arkitektur</h2>
-                <p> Organisering og Struktur af information</p>
+                <p> Organisering og Struktur af information.</p>
             
             </div>
                 <div id="iabillede">
@@ -112,7 +112,7 @@
         <a href="onepage.php"><div id="onepage"> <!--One page box start-->
             <div id="onetekst">
                 <h2> One Page</h2>
-                <p> Biotex Bent og One Page Projektet</p>
+                <p> Biotex Bent og One Page Projektet.</p>
             
             </div>
                 <div id="onebillede">
