f14aef855263679c2e0dd77af37dc066029f30ac
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-04 00:56:09 +0100

    Update refleksion.php

--- refleksion.php
+++ refleksion.php
@@ -4,17 +4,103 @@
         
         <title> Refleksion</title>
         <?php include "overordnet.inc" ?>    
-        <link rel="stylesheet" href="#"> <!--unikt Stylesheet mangler-->
+        <link rel="stylesheet" href="CSS/refleksion.css">
      </head>
     <body>
         
     <?php include "menu.inc" ?>    
         
         <div id="pagewrap">
-        
-      <h1> Refleksion</h1>
             
-        <p> Mere tekst på vej</p>    
+        <div id="refleksion2"> <!--refleksion start-->
+                <h1>Refleksion</h1>
+            
+                <p> Mine tanker og refleksioner omkring hjemmesiden kan du læse her. Jeg giver en forklaring på, hvorfor siden er struktureret som den er, og hvilke designmæssige overvejelser jeg har gjort mig. </p>
+            
+                <p> Derudover kan du læse mine refleksioner omkring arbejdet med denne læringsportfolio og 1. semester generelt.</p>
+                
+        </div> <!--refleksion slut-->     
+            
+            
+            <div id="portfolio"> <!--portfolio start-->
+                 <h2>Læringsportfolio</h2>
+            
+                <p> Til at starte med, var det vigtigt at få et overblik og struktur over selve indholdet, inden jeg begyndte at tænke i design og kode. Derfor var det allerførste jeg startede med, at få styr på notater og tekstforfatning.</p>
+            
+                <p> I den forbindelse var vi en lille selvstændig studiegruppe, som begyndte at gennemgå alt pensum fra 1. semester og skrive vores egne notater og små tekstfraser. Vi mødte hver dag på skolen og gennemgik pensum. Det var en meget konstruktiv og lærerig proces, hvor alle bød ind med inputs. Derudover var det en god måde at få genopfrisket de mange emner, som vi har været igennem det første halve år. Jeg tror på, at flere hjerner husker bedre end en. </p>
+            
+                <p> Personligt var dette en god arbejdsmetode for mig, da jeg fik startet tidligt op på eksamensopgaven. Det gav en behagelig ro, at der var styr på notater og en stor del af tekstforfatningen, inden selve designdelen og kodedelen skulle igangsættes. </p>
+            
+                <p> JJeg var meget bevidst om at udnytte den vejledning, som vores undervisere kunne tilbyde. Af den grund tog jeg et valg om at arbejde på skolen hver dag. I bund og grund brugte jeg meget af den studieteknik, som Mads Lønstrup havde præsenteret os for i Stoholm. Faste rutiner, hver dag.</p>
+        
+            </div> <!--portfolio slut-->   
+            
+        
+            
+               <div id="designdelen"> <!--Designdelen start-->
+                <h2> Designdelen </h2>
+            
+                <p> Inden jeg begyndte at kode, foretog jeg nogle overvejelser omkring, hvordan designet på min hjemmeside skulle se ud. Jeg var bevidst om, at jeg ville opbygge hjemmesiden i CSS Grids, derfor brugte jeg noget tid på at researche på forskellige hjemmesider, der var bygget op efter grids. Jeg brugte flere dage på at søge og spekulerede på, hvordan jeg skulle gribe designet an. Herefter gik jeg i gang med at designe wireframes til mobil, tablet og desktop. </p>
+            
+                <p> I flere dage gik jeg rundt og spekulerede på, hvordan jeg skulle gribe designet an. Indtil den slog mig en morgen, hvordan det skulle bygges op. Herefter gik jeg i gang med at designe wireframes til mobil, tablet og desktop. </p>
+            
+                <p> Siden blev bygget op omkring nogle menuer på forsiden, som skulle indeholde de forskellige fag vi har haft på 1. semester. Menuerne på forsiden skulle linke til nogle undermenuer, som skulle gøre det nemt at finde de forskellige emner vi har arbejdet med. </p>
+            
+                <p> I forhold til informationsarkitekturen har jeg arbejdet på at skabe en ”great ” informationsarkitektur - Det skulle gerne være indlysende for mig selv, hvor jeg finder de forskellige emner henne. Samtidig har jeg tænkt, at det i takt med uddannelsen, skulle være muligt at bygge videre på siden, og hele tiden tilføje nye fag, emner, projekter osv. – en slags logbog eller opslagsværk for mig selv.</p>
+            
+                <p> Jeg har også valgt at implementere en menubar øverst på både mobil, tablet og desktop, så man kan tilgå undermenuerne fra både menubaren og gridsene på forsiden. Mobil og Tablet er vist med en burgermenu, og desktopversionen har en fuld menubjælke.
+                Overordnet set er jeg selv tilfreds med hjemmesiden og den måde den fungerer på. Der er altid nogle ting, som jeg gerne vil have gjort anderledes, men det har alt sammen været en del af den samlede læringsproces.
+                </p>
+            
+                <p> Til min egen perspektivering, ville jeg f.eks. gerne have haft et større fokus på F-modellen i nogle af mine desktop-versioner, da det menneskelige øje skanner skærme på den måde. </p>
+                   
+                <p> Selvom jeg havde et rimeligt fast design i mine wireframes, har jeg alligevel været nødt til at tilpasse designet en smule løbende i processen. Især for at få billeder og tekster til at hænge sammen på en fornuftig måde.</p>   
+            
+        </div> <!--Designdelen slut--> 
+            
+            
+         <div id="kodedelen"> <!--kodedelen start-->
+                <h2> Kodedelen </h2>
+            
+                <p> Selve kodedelen har jeg arbejdet meget med på skolen i løbet af hele december. Jeg var fast besluttet på at jeg ville bygge siden op omkring CSS-grids. </p>
+            
+                <p> Først handlede det om at få kodet en forside der fungerede, og var responsive. Undervejs fik jeg vejledning af Niels, som var med til at holde mig på sporet, og sørge for at jeg fik startet min kodestruktur og kodeskik rigtigt op.</p>
+            
+                <p> Da jeg først havde fanget, hvordan CSS-grids fungerede på min side, kørte det egentlig bare derudad. Selvfølgelig har der været masser af udfordringer undervejs, men selve hovedgrebet fik jeg styr på ret tidligt i processen.</p>
+            
+                <p> I mine refleksioner omkring One Page-projektet skrev jeg, hvilke erfaringer vi havde gjort os med at kode HTML og CSS-grids. I One Page-projektet havde vi en alt for rodet kodestruktur, og ”ikke sigende” navne til vores billeder osv. Hvilket gjorde det meget forvirrende i længden.
+                Med denne her side, har jeg haft stort fokus på god kodeskik og ”sigende” navngivning af billeder og elementer. Nøglen til succes, når man arbejder med CSS-grids er, at man inddeler alt i div´er. På den måde kan man nemt styre det hele i CSS-grid.
+                </p>
+            
+        </div> <!--kodedelen slut-->     
+            
+            
+             <div id="sg"> <!--semestret generelt start-->
+                <h2> 1. Semester generelt</h2>
+            
+                <p> I starten af dette semester fik vi en opgave, som handlede om vores forventninger til studiet som multimediedesigner.
+                Her er et lille udpluk fra min rapport:
+                </p>
+            
+                <p> ”De dele af uddannelsen som tiltaler mig mest, er kombinationen af fagene og det praksisnære. Personligt, glæder jeg mig til at udvikle mig på nogle af de mere tekniske områder, såsom programmering og CSS. Det vil muligvis også være på de mere tekniske sider, at jeg vil blive udfordret - Netop af den grund, ser jeg frem til at komme i gang med dem. </p>
+            
+                <p> En af de større valg, jeg har truffet i forbindelsen med valg af uddannelse, var netop, at jeg ville udfordre mig selv på nogle nye områder f.eks. Programmering. For mit vedkommende handler det om at komme ud af min ”komfort zone” for at flytte mig personligt. Angående de mere tekniske fag, så vil min drivkraft udspringe fra det store perspektiv. Jeg kan sagtens forstå helheden, se hvordan tingene hænger sammen, og trække på eksempler fra erhvervslivet. Jeg vil dermed se fordelen i at være kreativ, men samtidig have evnen til at sætte handling bag ideerne. Eksemplet kunne være at få en god ide til en hjemmeside, og derefter være i stand udføre den rent teknisk.”
+                Jeg synes, at jeg flytter mig meget på det tekniske område i forhold til kodning, herunder HTML og CSS. Det faktum, at jeg har kodet en hjemmeside op fra bunden, har været en enorm givende oplevelse. Det har været en personlig sejr for mig at komme ud af min komfort zone.
+                </p>
+            
+                <p> Derudover er jeg blevet meget klogere på, hvordan man kan arbejder med forskellige processer, og hvordan man kan tvinge sig selv til at blive i nogle bestemte faser. Her tænker jeg især på Design Thinking projektet og One Page-projektet, som har været de to mest omfattende projekter vi har haft på 1. semester. </p>
+                 
+                
+                <p>I forhold til faget kommunikation er jeg blevet meget mere bevidst om, at kommunikation bliver brugt til alt. Det har været et fag, hvor jeg er blevet overrasket hver dag, og har fået noget ny viden med hjem. Jeg er blevet klar over, at kommunikation spiller en stor rolle i forhold til det at uddanne sig som multimediedesigner, da vi ikke kan undgå at kommunikere, uanset hvad vi gør. </p> 
+                
+                <p> Det har været spændende at arbejde med design på et både teoretisk og praktisk niveau. Jeg har været glad for kombinationen imellem at arbejde i forskellige programmer såsom, Illustrator, Photoshop osv. Samtidig med at vi har fået et designmæssigt opdrag i forhold til gestaltlove, farvelære, typografi osv.
+                Generelt synes jeg, at undervisningen på 1.semester har været ekstrem lærerig, og jeg glæder mig til at komme i gang med 2. semester.
+                </p> 
+                 
+        </div> <!--semestret generelt slut-->   
+        
+            
+         <div id="felt38"> &nbsp;</div> <!--holder footer nede på siden-->         
             
         </div> <!--pagewrap slut-->
         
