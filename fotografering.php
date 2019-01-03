4fa08796c8155fcd34934b7b7dc32289dafa7a17
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-04 00:55:39 +0100

    Update fotografering.php

--- fotografering.php
+++ fotografering.php
@@ -38,9 +38,9 @@
             
             <p class="tekst"> Hvis du tager et billede med lang lukketid af en forbikørende bil, vil resultatet blive sløret. Hvorimod tager du et billede af et stillestående objekt f.eks. et hus, kan man sagtens bruge lang lukketid og samtidig bevare skarpheden i billedet.</p>
             
-            <p class="tekst"> Man skal være opmærksom på ved lang lukketid, at bliver billedet nemt bliver overeksponeret og dermed helt hvidt. </p>
+            <p class="tekst"> Man skal være opmærksom på, at ved lang lukketid bliver billedet nemt overeksponeret og dermed helt hvidt. </p>
             
-            <p class="tekst"> Lang lukketid kan bruges som en cool effekt, hvis den bliver brugt korrekt. Nirvarna Album.</p>
+            <p class="tekst"> Lang lukketid kan bruges som en cool effekt, hvis den bliver brugt korrekt.</p>
             
         </div>  <!--Lukketid slut-->
             
@@ -49,7 +49,7 @@
             
             <p class="tekst"> ISO påvirker kameraets lysfølsomhed. Mængden af ISO påvirker mængden af lys, som billedet optager. Dette kan medvirke til et mindre skarpt billede.</p>
             
-            <p class="tekst"> Hvis du kombinerer brugen af ISO med lukketiden på kameraet, er det både muligt at fastfryse en genstand i bevægelse f.eks. en fodboldspiller, ved brug af lav lukketid og høj ISO. Den høje ISO sikrer at der kommer nok lys ind i billedet, og den lave lukketid sikrer at det ikke bliver et sløret billede, selvom der er bevægelse i det.</p>
+            <p class="tekst"> Hvis du kombinerer brugen af ISO med lukketiden på kameraet, er det både muligt at fastfryse en genstand i bevægelse f.eks. en fodboldspiller, ved brug af lav lukketid og høj ISO. Den høje ISO sikrer at der kommer nok lys ind i billedet, og den lave lukketid sikrer, at det ikke bliver et sløret billede, selvom der er bevægelse i det.</p>
             
         </div>  <!--Iso slut-->    
             
@@ -68,7 +68,9 @@
             
             <p class="tekst"> Når man skal tage billeder, er lyssætningen afgørende for, hvor gode billeder man får taget.</p>
             
-            <p class="tekst"> Man arbejder med hårdt og blødt lys. Farvetemperatur måles i Kelvin. Lysstyrken måles i Lumen.</p>
+            <p class="tekst"> Man arbejder med hårdt og blødt lys. Farvetemperaturen måles i Kelvin.</p>
+            
+            <p class="tekst"> Lysstyrken måles i Lumen.</p>
             
             <p class="tekst"> Blødt lys har en mere varm farve.</p>
                
