2252b9fdfd94bbdd431bf402343d7b67c6ca2eee
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-04 00:55:46 +0100

    Update historie.php

--- historie.php
+++ historie.php
@@ -41,7 +41,7 @@
             
             <p class="tekst"> I 1993 begynder udviklingen af den første brugbare browser NCSA Mosaic, som er med til at sætte gang i videreudviklingen af webbet.</p> 
             
-            <p class="tekst"> Det der kendetegner denne periode er, at forskellige individer, virksomheder, organisation eksperimenter med, hvordan webbet skal se ud. Der er ikke nogen officiel syntaks, eller guideline til hvordan webbet skal se ud. Denne periode er en form for det vilde vesten.</p> 
+            <p class="tekst"> Det der kendetegner denne periode er, at forskellige individer, virksomheder og organisationer eksperimenter med, hvordan webbet skal se ud. Der er ikke nogen officiel syntaks, eller guideline til, hvordan webbet skal se ud. Denne periode er en form for det vilde vesten.</p> 
         
             <p class="tekst">I denne periode var HTML-sproget og browserne teknisk begrænsende i forhold til design og layout. Websiderne var opbygget af simpel hypertekst, uden fokus på design. Stilarten på mange af siderne var det klassiske bogformat. Dette var ikke ligefrem med til at øge brugervenligheden på siderne.</p> 
              
@@ -75,7 +75,7 @@
             
             <h2>Semantisk Søgning (Fra 2000)</h2>
             
-            <p class="tekst">En af vigtige hændelser i denne periode er stiftelsen af søgemaskinen Google. Google hjalp brugerne med at finde det indhold, som de søgte på nettet. Google benyttede sig af en algoritme, som rankede siderne efter semantisk betydning. Det medførte til en helt ny industri indenfor søgemaskine optimering, bedre kendt som SEO.</p> 
+            <p class="tekst">En af vigtige hændelser i denne periode er stiftelsen af søgemaskinen Google. Google hjalp brugerne med at finde det indhold, som de søgte på nettet. Google benyttede sig af en algoritme, som rankede siderne efter semantisk betydning. Det medførte en helt ny industri indenfor søgemaskine optimering, bedre kendt som SEO.</p> 
             
             <p class="tekst"> Googles tanke om at webbet skulle være et sted, hvor alle kunne finde og bidrage med nyt indhold, passede i fin tråd med Tim Berners Lees grundide omkring webbet.</p> 
             
@@ -92,7 +92,7 @@
             
             <p class="tekst"> En af de første sociale medier er mediet MySpace, som har fokus på musik. MySpace bliver populært, da musikere og kunstnere nu selv kan udgive deres egen musik uden om pladeselskaber, hvilket revolutionerer hele musikbranchen på meget få år. MySpace bliver et forum, hvor folk kan finde nichemusik, som ikke bliver spillet i mainstream radio og tv. Dette tiltrækker store grupper af mennesker, som interesser sig for f.eks. Metal, Rap, Hiphop og mange andre genrer, som på dette tidspunkt var nichegenre. </p> 
             
-            <p class="tekst"> I forbindelse med at Facebook opnåede op for brugere i september 2005, oplevede MySpace en voldsom nedgang i brugere. Forskellen på MySpace og Facebook, var at på Facebook, kunne man følge sine venner, spille spil, læse nyheder osv. Muligheden for at kommunikere på Facebook var langt bredere end på MySpace. På Facebook kunne man deltage i debatter, skrive opslag på sin væg, skrive privatbeskeder osv.</p> 
+            <p class="tekst"> I forbindelse med at Facebook opnåede op for brugere i september 2005, oplevede MySpace en voldsom nedgang i brugere. Forskellen på MySpace og Facebook var, at på Facebook, kunne man følge sine venner, spille spil, læse nyheder osv. Muligheden for at kommunikere på Facebook var langt bredere end på MySpace. På Facebook kunne man deltage i debatter, skrive opslag på sin væg, skrive privatbeskeder osv.</p> 
             
         </div>  <!--Sociale Netværk slut--> 
             
@@ -102,7 +102,7 @@
             
             <h2>Det Mobile Web (Fra 2010)</h2>
             
-            <p class="tekst">I 2007 lancerede Apple den første smartphone, som kunne tilgå internettet. Det revolutionerende ved IPhone og senere Tablet (2010) var hele ideen med touchscreen. Man behøvede ikke længere et tastetur, og på den måde blev telefonen en ny form for computer.</p> 
+            <p class="tekst">I 2007 lancerede Apple den første smartphone, som kunne tilgå internettet. Det revolutionerende ved iPhone og senere Tablet (2010) var hele ideen med touchscreen. Man behøvede ikke længere et tastetur, og på den måde blev telefonen en ny form for computer.</p> 
             
             <p class="tekst"> I løbet af få år blev de mobile webbrugere i overtal, og i 2010 var der flere brugere på det mobile web. Brugerne nægtede at anvende hjemmesider, som ikke var opdateret til telefonformatet. Det var i den forbindelse at responsive design, så dagens lys.  </p> 
             
@@ -116,12 +116,12 @@
             <h2> Hvad er forskellen på begreberne "Internettet" og "World Wide Web" </h2> 
             
             <p class="tekst"> Internettet er lig med infrasturktur = World Wide Web er en applikation på den infrastruktur. 
-            (eksempel Metafor = Vejnettet er internettet: Busser, biler er applikationer på vejnettet).
+            (eksempel Metafor = Vejnettet er internettet: Busser og biler er applikationer på vejnettet).
             </p> 
             
             <h2>Hvad kendetegner udviklingen af internettet?</h2>
             
-            <p class="tekst"> Protokoller har været afgørende for internettet. Det er et regelsæt (en aftale), som gør at vi kommunikerer på det samme sprog. </p> 
+            <p class="tekst"> Protokoller har været afgørende for internettet. Det er et regelsæt (en aftale), som gør, at vi kommunikerer på det samme sprog. </p> 
                 
             <h2>Hvorfra kommer begrebet "Inter-net" og hvad hentyder det til?</h2>
             
@@ -129,7 +129,7 @@
             
             <h2>Hvornår startede udviklingen af internettet?</h2>
             
-            <p class="tekst">Udviklingen af internettet startede tilbage i 1957. Det startede under den kolde krig, da Sovjetunionen, som den første nation i verden, sendte den ubemandede rumsonde Sputnik i kredsløb rundt om jorden. I 1966 begyndte man arbejde på ARPANET, som var et samarbejde imellem lande som England, USA og Frankring. De forskellige interesser heri var militære, videnskabelige og kommercielle</p>
+            <p class="tekst">Udviklingen af internettet startede tilbage i 1957. Det startede under den kolde krig, da Sovjetunionen, som den første nation i verden, sendte den ubemandede rumsonde Sputnik i kredsløb rundt om jorden. I 1966 begyndte man arbejdet på ARPANET, som var et samarbejde imellem lande som England, USA og Frankring. De forskellige interesser heri var militære, videnskabelige og kommercielle</p>
             
             <h2> Hvad er "information overload"</h2>
             
@@ -137,34 +137,34 @@
             
             <h2> Hvad var ideen bag "World Palace of Knowledge"</h2>
             
-            <p class="tekst">Folkene bag World Palace of Knowledge var Paul Otlet og Henri LaFontaine som havde en vision om, at verdensfred kunne opnås ved global vidensdeling. De begyndte at samle og organisere viden, i verdens første manuelle søgemaskine. 
+            <p class="tekst">Folkene bag World Palace of Knowledge var Paul Otlet og Henri LaFontaine, der havde en vision om, at verdensfred kunne opnås ved global vidensdeling. De begyndte at samle og organisere viden, i verdens første manuelle søgemaskine. 
 
             De skabte i bund og grund et kæmpe arkiv med over 50 millioner dokumenter og videofilm. Når almindelige mennesker sendte spørgsmål til World Palace of Knowledge, var der ansatte, som fandt alt information der var på området, men det helt unikke var, at de også fandt relateret artikler til det specifikke emne, altså en form for linking. 
             </p>
             
             <h2>Hvad var ideen bag "The Memex"?</h2>
             
-            <p class="tekst">Vannevar Bush.
+            <p class="tekst">
             Det samme princip som ”World Palace of Knowledge” men mere automatisk. Brug af keywords, for at finde de rigtige informationer, det gjorde man ved at benytte association (Linking). 
             </p>    
             
             <h2>Hvem var Douglas Engelbart og hvad er han kendt for?</h2>
             
-            <p class="tekst">Pioneer indenfor udviklingen af computer tilbage i slutningen af 60erne. Hans forskerteam opfandt bl.a. ”The Mouse” musen. De forsøgte at lave værktøjerne til i praksis at kunne dele information.</p> 
+            <p class="tekst">Pioneer indenfor udviklingen af computeren tilbage i slutningen af 60erne. Hans forskerteam opfandt bl.a. ”The Mouse” musen. De forsøgte at lave værktøjerne til i praksis, at kunne dele information.</p> 
             
             <h2>Hvem opfandt begrebet "Hypertext" og hvad kunne det første Hypertext System.</h2>
             
-            <p class="tekst">-	Ted Nelson – Man kunne skrive ord og åbne flere vinduer. Han anså som alt tekst forbundet. Deraf navnet Hypertext. </p> 
+            <p class="tekst">-	Ted Nelson – Man kunne skrive ord og åbne flere vinduer. Han anså alt tekst som forbundet. Deraf navnet Hypertext. </p> 
             
             <h2>Hvordan inspirerede Tim's far's artikel Tim?</h2>
             
-            <p class="tekst">Den inspirerede ham til, hvordan en computer kan etablere links ligesom den menneskelige hjerne gør. Hjernen linker ting som har en fælles funktion, selvom de i første omgang kan virke fuldstændig forskellige. F.eks. en hest - Det kan være en hest på en mark, det kan være en hest på et skakbræt, det kan være en hest i gymnastiksal. 
-            Det var denne sammenhæng mellem tekst, som man kalder hypertekst der inspirerede Tim Berners Lee. Han arbejdede med ideen om at lære computeren at læse hypertekst. 
+            <p class="tekst">Den inspirerede ham til, hvordan en computer kan etablere links ligesom den menneskelige hjerne gør. Hjernen linker ting som har en fælles funktion, selvom de i første omgang kan virke fuldstændig forskellige. F.eks. en hest - Det kan være en hest på en mark, det kan være en hest på et skakbræt, det kan være en hest i en gymnastiksal. 
+            Det var denne sammenhæng mellem tekst, som man kalder hypertekst, der inspirerede Tim Berners Lee. Han arbejdede med ideen om at lære computeren at læse hypertekst. 
             </p> 
             
             <h2>Hvad var programmet Enquire</h2>
             
-            <p class="tekst">Da Tim Berners Lee kom til Cern i 1980, ville han gerne have et overblik over, hvem der stod for hvad, og hvilke computerprogrammer de brugte. For at få et overblik skabte han Enquire, som var et program der kunne sammenligne dokumenter omkring ansatte, deres projekter, og den software de brugte. Enquire kunne tjekke om dokumenter indeholdte nogle specifikke keywords. Hvis det gjorde, var deres lokation i dokumentet inter linked.</p> 
+            <p class="tekst">Da Tim Berners Lee kom til Cern i 1980, ville han gerne have et overblik over, hvem der stod for hvad, og hvilke computerprogrammer de brugte. For at få et overblik skabte han Enquire, som var et program der kunne sammenligne dokumenter omkring ansatte, deres projekter, og den software de brugte. Enquire kunne tjekke om dokumenterne indeholdte nogle specifikke keywords. Hvis det gjorde, var deres lokation i dokumentet inter linked.</p> 
             
             <h2>Hvad var problemet med ARPANET?</h2>
             
@@ -184,7 +184,7 @@
             
             <h2>Hvem var Marc Andreasen?</h2>
             
-            <p class="tekst">Marc Andreasen var en ung studerende som skabte browseren Mosaic. Mosaic indeholdte både hyperlinks og billeder. </p> 
+            <p class="tekst">Marc Andreasen var en ung studerende, som skabte browseren Mosaic. Mosaic indeholdte både hyperlinks og billeder. </p> 
             
         </div>  <!--Historie om Internettet slut-->
             
