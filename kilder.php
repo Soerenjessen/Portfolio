2c8a01cf7cc10efd72a69d096f506a59ffdbfdf4
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-04 00:55:53 +0100

    Update kilder.php

--- kilder.php
+++ kilder.php
@@ -4,7 +4,7 @@
         
         <title> Kilder</title>
         <?php include "overordnet.inc" ?>    
-        <link rel="stylesheet" href="CSS/webudvikling.css">
+        <link rel="stylesheet" href="CSS/kilder.css">
 
      </head>
     <body>
@@ -14,8 +14,30 @@
         <div id="pagewrap">
         
         <div id="kilder"> <!--Kilder start-->
-                <h1> Kilder</h1>
-                <p> Kildeliste og bibliografi</p>
+                <h1> Kildeliste og bibliografi</h1>
+            
+                <p> Daugaard, 2018, Kommunikation MMD1 11092018 kommunikationsteori</p>
+            
+                <p> Busch, 2015, Kommunikation i multimediedesign</p>
+                
+                <p> Daugaard, 2018, Kommunikation Præsentationsteknik - MMD 18092018</p>
+            
+                <p> https://www.religion.dk/leksikon/positivisme</p>
+            
+                <p> https://videnskab.dk/kultur-samfund/hvad-er-hermeneutik</p>
+            
+                <p> Daugaard, 2018, MMD brugertest 10102018</p>
+            
+                <p> Daugaard, 2018, S.15, Forretningsforståelse MMD 07112018</p>
+            
+                <p> Østergaard, 2018, introduktion til fotografering</p>
+            
+                <p> Bogner, 2018, S.10 ,04_gestaltlovene.pdf</p>
+            
+                <p> videnskab.dk/sporg-videnskaben/hvordan-pavirker-farver-os</p>
+            
+                <p> (Bogner, 2018, farvelaere.pdf)</p>
+            
         </div> <!--Kilder slut-->
             
             <div id="felt18"> &nbsp;</div> <!--holder footer nede på siden-->     
