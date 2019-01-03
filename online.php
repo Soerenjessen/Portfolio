0fe28230205a760b31761e48e4cd4324d15564b1
Soren Jessen <soer64b9@student.eadania.dk>
2019-01-04 00:55:58 +0100

    Update online.php

--- online.php
+++ online.php
@@ -18,13 +18,13 @@
             
             <p class="tekst"> Med Carsten Guldbord beskæftigede vi os med forskellige discipliner indenfor Online Marketing. Alt lige fra Facebook til Google adwords. Det var en spændende dag, hvor vi fik et lynhurtigt kursus/gennemgang af de erfaringer Carsten havde gjort sig i forhold til sit eget Galleri.</p> 
             
-            <p class="tekst"> Vi arbejde med SEO-strategier, adwords, linkbuilding, nyhedsmails, affiliate marketing og andre metoder til hvordan man kan genere mere trafik ind på ens side. </p> 
+            <p class="tekst"> Vi arbejdede med SEO-strategier, adwords, linkbuilding, nyhedsmails, affiliate marketing og andre metoder til, hvordan man kan genere mere trafik ind på ens side. </p> 
             
-            <p class="tekst"> Derudover beskæftigede vi os med konverteringsrater – Vi kiggede på, hvordan man kan øge sin konverteringsrate ved at have fokus på brugervenlighed. Sagt med andre ord, hvordan får vi de potentielle kunder der besøger vores side, til at færdiggøre en handel, altså ende i en konvertering. </p> 
+            <p class="tekst"> Derudover beskæftigede vi os med konverteringsrater – Vi kiggede på, hvordan man kan øge sin konverteringsrate ved at have fokus på brugervenlighed. Sagt med andre ord, hvordan får vi de potentielle kunder, der besøger vores side, til at færdiggøre en handel, altså ende i en konvertering. </p> 
               
-            <p class="tekst"> Derforuden havde vi fokus på, hvordan man kan optimere en hjemmeside, således at den performer bedre. Det kan man gøre ved f.eks. at have fokus på loadhastighed. </p> 
+            <p class="tekst"> Derforuden havde vi fokus på, hvordan man kan optimere en hjemmeside, således at den performer bedre. Det kan man gøre ved f.eks. at have fokus på load-hastighed. </p> 
             
-            <p class="tekst"> En af de mest primære ting, som vi arbejdede med var, hvordan vi kan måle de tiltag som vi foretager os i forbindelse med online marketing.</p> 
+            <p class="tekst"> En af de mest primære ting, som vi arbejdede med var, hvordan vi kan måle de tiltag, vi foretager os i forbindelse med online marketing.</p> 
             
         </div>  <!--Online markedsføring slut-->    
             
@@ -33,24 +33,24 @@
         <div id="some"> <!--Sociale Medier start-->
             <h2>Sociale Medier</h2> 
             
-            <p class="tekst"> Sociale medier kan være en guldgruppe, hvis man forstår at kommunikere rigtigt til sin målgruppe. Der er mange virksomheder, som begår den fejl, at de kommunikerer alt for formelt og kedeligt på sociale medier. Her skal man huske på, at folk ikke gider se/høre omkring virksomheder, derfor er man som virksomhed nødt til at tænke over hvordan man sælger sit budskab. </p> 
+            <p class="tekst"> Sociale medier kan være en guldgruppe, hvis man forstår at kommunikere rigtigt til sin målgruppe. Der er mange virksomheder, som begår den fejl, at de kommunikerer alt for formelt og kedeligt på de sociale medier. Her skal man huske på, at folk ikke gider se/høre omkring virksomheder, derfor er man som virksomhed nødt til at tænke over hvordan man sælger sit budskab. </p> 
             
-            <p class="tekst"> Derudover skal man som virksomhed være opmærksom på, at der er forskel på, hvordan man skal agere på de forskellige sociale medier. DVS at det ikke den samme strategi, der skal bruges på alle sociale medier. Der kan være stor forskel på at bruge Instagram og Facebook. </p> 
+            <p class="tekst"> Derudover skal man som virksomhed være opmærksom på, at der er forskel på, hvordan man skal agere på de forskellige sociale medier. Dvs. at det ikke er den samme strategi, der skal bruges på alle sociale medier. Der kan være stor forskel på at bruge Instagram og Facebook. </p> 
             
-            <p class="tekst"> Instagram er et helt andet medie end facebook, her kan man sagtens foretage 2-3 opslag om dagen, hvorimod på Facebook vil du blive betragtet som irriterende, hvis du foretog 2-3 opsalg om dagen.</p> 
+            <p class="tekst"> Instagram er et helt andet medie end Facebook, her kan man sagtens foretage 2-3 opslag om dagen, hvorimod på Facebook vil du blive betragtet som irriterende, hvis du foretog 2-3 opslag om dagen.</p> 
               
-            <p class="tekst"> Man skal være skarp, og kende fordele og ulemperne ved de forskellige sociale medier. Gør man ikke det, får man ikke noget særligt ud af det. Det kan derfor være en fordel at entrere sig med nogle mennesker, som ved meget om sociale medier. </p> 
+            <p class="tekst"> Man skal være skarp, og kende sine fordele og ulemperne ved de forskellige sociale medier. Gør man ikke det, får man ikke noget særligt ud af det. Det kan derfor være en fordel at entrere sig med nogle mennesker, som ved meget om sociale medier. </p> 
             
         </div>  <!--Sociale Medier slut-->          
                 
             
             
         <div id="konrate"> <!--konverteringsrate start-->
-            <h2>konverteringsrate</h2> 
+            <h2>Konverteringsrate</h2> 
             
-            <p class="tekst"> Konverteringsraten for danske e-shops er ca. 2% - Dvs ud 100 besøgende kunder på en webshop, er det kun 2 af dem, som ender med at foretage en handel (konvertering). 70% af kunderne falder fra i betalingsprocessen, hvilket er rigtig mange potentielle konvertering, som virksomhederne går glip af.</p> 
+            <p class="tekst"> Konverteringsraten for danske e-shops er ca. 2% - Dvs. ud 100 besøgende kunder på en webshop, er det kun 2 af dem, som ender med at foretage en handel (konvertering). 70% af kunderne falder fra i betalingsprocessen, hvilket er rigtig mange potentielle konvertering, som virksomhederne går glip af.</p> 
             
-            <p class="tekst">Man kan sagtens forestille sig, hvor mange penge det drejer sig om, hvis man sidder i nogle større webshopper som omsætter for f.eks. en million i måneden. De kan være meget interesseret i at forbedre deres konverteringsrate bare med 1-2%, da det virkelig kan mærkes på bundlinjen.  </p> 
+            <p class="tekst">Man kan sagtens forestille sig, hvor mange penge det drejer sig om, hvis man sidder i nogle større webshopper, som omsætter for f.eks. en million i måneden. De kan være meget interesseret i at forbedre deres konverteringsrate bare med 1-2%, da det virkelig kan mærkes på bundlinjen.  </p> 
         
             
         </div>  <!--konverteringsrate slut-->      
